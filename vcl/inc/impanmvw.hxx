LibreOffice/core

vcl: change drawToPos() to drawToIndex()
Change-Id: I040b6d17cc8bb9d867aa771814464df19615f195

vcl/inc/impanmvw.hxx
@@ -60,7 +60,7 @@ private:
     vcl::Region     maClip;
     VclPtr<VirtualDevice>  mpBackground;
     VclPtr<VirtualDevice>  mpRestore;
-    sal_uLong       mnActPos;
+    sal_uLong       mnActIndex;
     Disposal        meLastDisposal;
     bool            mbIsPaused;
     bool            mbIsMarked;
@@ -75,8 +75,8 @@ private:
                                   OutputDevice* pFirstFrameOutDev = nullptr );
 
     bool            matches( OutputDevice* pOut, long nCallerId ) const;
-    void            drawToPos( sal_uLong nPos );
-    void            draw( sal_uLong nPos, VirtualDevice* pVDev=nullptr );
+    void            drawToIndex(sal_uLong nIndex);
+    void            draw( sal_uLong nIndex, VirtualDevice* pVDev=nullptr );
     void            repaint();
     AInfo*          createAInfo() const;
 

chrissherlock/libreoffice-experimental

Change-Id: Iaf102e529cdfd3d758e4e6976f6a99330d8fc64b

vcl/inc/impanmvw.hxx
@@ -58,7 +58,7 @@ private:
     vcl::Region maClip;
     VclPtr<VirtualDevice> mpBackground;
     VclPtr<VirtualDevice> mpRestore;
-    sal_uLong mnActPos;
+    sal_uLong mnActIndex;
     Disposal meLastDisposal;
     bool mbIsPaused;
     bool mbIsMarked;
@@ -71,7 +71,7 @@ public:
     ~AnimationRenderer();
 
     bool matches(const OutputDevice* pOut, long nCallerId) const;
-    void drawToPos(sal_uLong nPos);
+    void drawToIndex(sal_uLong nPos);
     void draw(sal_uLong nPos, VirtualDevice* pVDev = nullptr);
     void repaint();
     AInfo* createAInfo() const;