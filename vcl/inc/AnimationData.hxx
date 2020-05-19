LibreOffice/core

vcl: AInfo -> AnimationData
Change-Id: I4a4f8e2e346010edd1fde004eb9f14b67fc8487a

include/vcl/animate/AnimationData.hxx
@@ -1,0 +1,41 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef INCLUDED_VCL_ANIMATE_ANIMATIONDATA_HXX
+#define INCLUDED_VCL_ANIMATE_ANIMATIONDATA_HXX
+
+#include <tools/gen.hxx>
+
+#include <vcl/vclptr.hxx>
+
+class OutputDevice;
+
+struct AnimationData
+{
+    Point aStartOrg;
+    Size aStartSize;
+    VclPtr<OutputDevice> pOutDev;
+    void* pAnimationRenderer;
+    long nCallerId;
+    bool bPause;
+
+    AnimationData();
+};
+
+#endif // INCLUDED_VCL_ANIMATE_ANIMATIONDATA_HXX

vcl/source/animate/Animation.cxx
@@ -289,27 +289,29 @@ IMPL_LINK_NOARG(Animation, ImplTimeoutHdl, Timer*, void)
 
         if (maNotifyLink.IsSet())
         {
-            std::vector<std::unique_ptr<AInfo>> aAInfoList;
-            // create AInfo-List
-            for (auto const& i : maAnimationRenderers)
-                aAInfoList.emplace_back(i->createAInfo());
+            std::vector<std::unique_ptr<AnimationData>> aAnimationDataItems;
+            // create AnimationData-List
+            for (auto const& rAnimDataItem : maAnimationRenderers)
+                aAnimationDataItems.emplace_back(rAnimDataItem->createAnimationData());
 
             maNotifyLink.Call(this);
 
-            // set view state from AInfo structure
-            for (auto& pAInfo : aAInfoList)
+            // set view state from AnimationData structure
+            for (auto& pItem : aAnimationDataItems)
             {
-                if (!pAInfo->pViewData)
+                if (!pItem->pViewData)
                 {
-                    pView = new AnimationRenderer(this, pAInfo->pOutDev, pAInfo->aStartOrg,
-                                                  pAInfo->aStartSize, pAInfo->nCallerId);
+                    pView = new AnimationRenderer(this, pItem->pOutDev, pItem->aStartOrg,
+                                                  pItem->aStartSize, pItem->nCallerId);
 
                     maAnimationRenderers.push_back(std::unique_ptr<AnimationRenderer>(pView));
                 }
                 else
-                    pView = static_cast<AnimationRenderer*>(pAInfo->pViewData);
+                {
+                    pView = static_cast<AnimationRenderer*>(pItem->pViewData);
+                }
 
-                pView->pause(pAInfo->bPause);
+                pView->pause(pItem->bPause);
                 pView->setMarked(true);
             }
 
@@ -683,7 +685,7 @@ SvStream& ReadAnimation(SvStream& rIStm, Animation& rAnimation)
     return rIStm;
 }
 
-AInfo::AInfo()
+AnimationData::AnimationData()
     : pOutDev(nullptr)
     , pViewData(nullptr)
     , nCallerId(0)

vcl/source/gdi/impanmvw.cxx
@@ -314,18 +314,18 @@ void AnimationRenderer::repaint()
     mbIsPaused = bOldPause;
 }
 
-AInfo* AnimationRenderer::createAInfo() const
+AnimationData* AnimationRenderer::createAnimationData() const
 {
-    AInfo* pAInfo = new AInfo;
+    AnimationData* pAnimationData = new AnimationData;
 
-    pAInfo->aStartOrg = maPt;
-    pAInfo->aStartSize = maSz;
-    pAInfo->pOutDev = mpRenderContext;
-    pAInfo->pViewData = const_cast<AnimationRenderer *>(this);
-    pAInfo->nCallerId = mnCallerId;
-    pAInfo->bPause = mbIsPaused;
+    pAnimationData->aStartOrg = maPt;
+    pAnimationData->aStartSize = maSz;
+    pAnimationData->pOutDev = mpRenderContext;
+    pAnimationData->pViewData = const_cast<AnimationRenderer *>(this);
+    pAnimationData->nCallerId = mnCallerId;
+    pAnimationData->bPause = mbIsPaused;
 
-    return pAInfo;
+    return pAnimationData;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

chrissherlock/libreoffice-experimental

vcl/inc/AnimationData.hxx
@@ -1,0 +1,41 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef INCLUDED_VCL_INC_ANIMATIONDATA_HXX
+#define INCLUDED_VCL_INC_ANIMATIONDATA_HXX
+
+#include <tools/gen.hxx>
+
+#include <vcl/vclptr.hxx>
+
+class OutputDevice;
+
+struct AnimationData
+{
+    Point aStartOrg;
+    Size aStartSize;
+    VclPtr<OutputDevice> pOutDev;
+    void* pAnimationRenderer;
+    long nCallerId;
+    bool bPause;
+
+    AnimationData();
+};
+
+#endif // INCLUDED_VCL_ANIMATE_ANIMATIONDATA_HXX

vcl/source/animate/Animation.cxx
@@ -29,6 +29,7 @@
 #include <vcl/dibtools.hxx>
 #include <vcl/BitmapColorQuantizationFilter.hxx>
 
+#include <AnimationData.hxx>
 #include <impanmvw.hxx>
 
 sal_uLong Animation::mnAnimCount = 0;
@@ -285,28 +286,29 @@ IMPL_LINK_NOARG(Animation, ImplTimeoutHdl, Timer*, void)
 
         if (maNotifyLink.IsSet())
         {
-            std::vector<std::unique_ptr<AInfo>> aAInfoList;
-            // create AInfo-List
+            std::vector<std::unique_ptr<AnimationData>> aAnimationDataList;
+            // create AnimationData-List
             for (auto const& i : maAnimationRenderers)
-                aAInfoList.emplace_back(i->createAInfo());
+                aAnimationDataList.emplace_back(i->createAnimationData());
 
             maNotifyLink.Call(this);
 
-            // set view state from AInfo structure
-            for (auto& pAInfo : aAInfoList)
+            // set view state from AnimationData structure
+            for (auto& pAnimationData : aAnimationDataList)
             {
                 AnimationRenderer* pRenderer = nullptr;
-                if (!pAInfo->pViewData)
+                if (!pAnimationData->pAnimationRenderer)
                 {
-                    pRenderer = new AnimationRenderer(this, pAInfo->pOutDev, pAInfo->aStartOrg,
-                                                      pAInfo->aStartSize, pAInfo->nCallerId);
+                    pRenderer = new AnimationRenderer(
+                        this, pAnimationData->pOutDev, pAnimationData->aStartOrg,
+                        pAnimationData->aStartSize, pAnimationData->nCallerId);
 
                     maAnimationRenderers.push_back(std::unique_ptr<AnimationRenderer>(pRenderer));
                 }
                 else
-                    pRenderer = static_cast<AnimationRenderer*>(pAInfo->pViewData);
+                    pRenderer = static_cast<AnimationRenderer*>(pAnimationData->pAnimationRenderer);
 
-                pRenderer->pause(pAInfo->bPause);
+                pRenderer->pause(pAnimationData->bPause);
                 pRenderer->setMarked(true);
             }
 
@@ -622,8 +624,9 @@ SvStream& ReadAnimation(SvStream& rIStm, Animation& rAnimation)
     // If the BitmapEx at the beginning have already been read (by Graphic)
     // we can start reading the AnimationBitmaps right away
     if ((nAnimMagic1 == 0x5344414e) && (nAnimMagic2 == 0x494d4931) && !rIStm.GetError())
+    {
         bReadAnimations = true;
-    // Else, we try reading the Bitmap(-Ex)
+    } // Else, we try reading the Bitmap(-Ex)
     else
     {
         rIStm.Seek(nStmPos);
@@ -676,9 +679,9 @@ SvStream& ReadAnimation(SvStream& rIStm, Animation& rAnimation)
     return rIStm;
 }
 
-AInfo::AInfo()
+AnimationData::AnimationData()
     : pOutDev(nullptr)
-    , pViewData(nullptr)
+    , pAnimationRenderer(nullptr)
     , nCallerId(0)
     , bPause(false)
 {

vcl/source/gdi/impanmvw.cxx
@@ -24,6 +24,7 @@
 #include <vcl/window.hxx>
 #include <tools/helpers.hxx>
 
+#include <AnimationData.hxx>
 #include <window.h>
 
 AnimationRenderer::AnimationRenderer( Animation* pParent, OutputDevice* pOut,
@@ -304,18 +305,18 @@ void AnimationRenderer::repaint()
     mbIsPaused = bOldPause;
 }
 
-AInfo* AnimationRenderer::createAInfo() const
+AnimationData* AnimationRenderer::createAnimationData() const
 {
-    AInfo* pAInfo = new AInfo;
+    AnimationData* pAnimationData = new AnimationData;
 
-    pAInfo->aStartOrg = maPt;
-    pAInfo->aStartSize = maSz;
-    pAInfo->pOutDev = mpRenderContext;
-    pAInfo->pViewData = const_cast<AnimationRenderer *>(this);
-    pAInfo->nCallerId = mnCallerId;
-    pAInfo->bPause = mbIsPaused;
+    pAnimationData->aStartOrg = maPt;
+    pAnimationData->aStartSize = maSz;
+    pAnimationData->pOutDev = mpRenderContext;
+    pAnimationData->pAnimationRenderer = const_cast<AnimationRenderer *>(this);
+    pAnimationData->nCallerId = mnCallerId;
+    pAnimationData->bPause = mbIsPaused;
 
-    return pAInfo;
+    return pAnimationData;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */