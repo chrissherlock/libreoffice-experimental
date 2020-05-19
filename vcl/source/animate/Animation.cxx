LibreOffice/core

vcl: rename bGlobalPause to bIsGloballyPaused
Change-Id: I9a67d307f5e347da9da272572fc267c91cc2b815

vcl/source/animate/Animation.cxx
@@ -328,17 +328,17 @@ void Animation::DeleteUnmarkedRenderers()
 
 bool Animation::CanResetMarkedRenderers()
 {
-    bool bGlobalPause = true;
+    bool bIsGloballyPaused = true;
 
     for (auto& rItem : maAnimationRenderers)
     {
         if (!rItem->isPause())
-            bGlobalPause = false;
+            bIsGloballyPaused = false;
 
         rItem->setMarked(false);
     }
 
-    return bGlobalPause;
+    return bIsGloballyPaused;
 }
 
 bool Animation::IsTimeoutSetup() { return maTimeoutNotifier.IsSet(); }
@@ -429,11 +429,11 @@ IMPL_LINK_NOARG(Animation, ImplTimeoutHdl, Timer*, void)
 {
     if (!maAnimationFrames.empty())
     {
-        bool bGlobalPause = CanSendTimeout();
+        bool bIsGloballyPaused = CanSendTimeout();
 
         if (maAnimationRenderers.empty())
             Stop();
-        else if (bGlobalPause)
+        else if (bIsGloballyPaused)
             ImplRestartTimer(10);
         else
             RenderNextFrame();

chrissherlock/libreoffice-experimental

Change-Id: I677fb74dbc3cbb1945f01aed902cce6be6bfbc28

vcl/source/animate/Animation.cxx
@@ -417,11 +417,11 @@ IMPL_LINK_NOARG(Animation, ImplTimeoutHdl, Timer*, void)
 {
     if (!maAnimationFrames.empty())
     {
-        bool bGlobalPause = SendTimeout();
+        bool bIsGloballyPaused = SendTimeout();
 
         if (maAnimationRenderers.empty())
             Stop();
-        else if (bGlobalPause)
+        else if (bIsGloballyPaused)
             ImplRestartTimer(10);
         else
             RenderNextFrame(mnFrameIndex);