eaabaacfd974e8125dbbbeffcb24e7424a2b01a2
Chris Sherlock <chris.sherlock79@gmail.com>
2020-08-09 17:03:49 +1000

    cppuhelper: implement XWeak interface functionality
    Change-Id: Iac772997a532e0b5bf63f24375212ee39bc9762e

--- cppuhelper/qa/interface/test_serviceimpl.cxx
+++ cppuhelper/qa/interface/test_serviceimpl.cxx
@@ -11,18 +11,64 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/plugin/TestPlugIn.h>
 
+#include <osl/mutex.hxx>
 #include <rtl/ustring.hxx>
 #include <cppuhelper/supportsservice.hxx>
 #include <cppuhelper/queryinterface.hxx>
 
+#include <com/sun/star/uno/XWeak.hpp>
+#include <com/sun/star/uno/XAdapter.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/lang/XTypeProvider.hpp>
 
-class CoreComponentInterfaces : public css::lang::XServiceInfo, public css::lang::XTypeProvider
+class CoreComponentInterfaces;
+
+static osl::Mutex& getWeakMutex()
 {
+    static osl::Mutex* spMutex = new osl::Mutex();
+    return *spMutex;
+}
+
+class Adapter : public css::uno::XAdapter
+{
+public:
+    explicit Adapter(CoreComponentInterfaces* pWeakObj)
+        : mnRefCount(0)
+        , mpWeakObject(pWeakObj)
+    {
+    }
+
+    virtual ~Adapter() {}
+
+    Adapter(Adapter const&) = delete;
+    Adapter const& operator=(Adapter const&) = delete;
+
+    // XInterface
+    virtual css::uno::Any queryInterface(css::uno::Type const& rType) override;
+    virtual void acquire() override;
+    virtual void release() override;
+
+    // XAdapter
+    virtual css::uno::Reference<css::uno::XInterface> queryAdapted() override;
+    virtual void addReference(css::uno::Reference<css::uno::XReference> const& xRef) override;
+    virtual void removeReference(css::uno::Reference<css::uno::XReference> const& xRef) override;
+
+private:
+    oslInterlockedCount mnRefCount;
+    CoreComponentInterfaces* mpWeakObject;
+    std::vector<css::uno::Reference<css::uno::XReference>> maReferences;
+};
+
+class CoreComponentInterfaces : public css::lang::XServiceInfo,
+                                public css::lang::XTypeProvider,
+                                public css::uno::XWeak
+{
+    friend class Adapter;
+
 public:
     CoreComponentInterfaces()
         : mnRefCount(0)
+        , mpAdapter(nullptr)
     {
     }
 
@@ -42,9 +88,67 @@ public:
     virtual css::uno::Sequence<css::uno::Type> getTypes();
     virtual css::uno::Sequence<sal_Int8> getImplementationId(); // deprecated
 
+    // XWeak
+    virtual css::uno::Reference<css::uno::XAdapter> queryAdapter();
+
 private:
     oslInterlockedCount mnRefCount;
+    Adapter* mpAdapter;
 };
+
+css::uno::Any Adapter::queryInterface(css::uno::Type const& rType)
+{
+    return cppu::queryInterface(rType, this);
+}
+
+void Adapter::acquire() { osl_atomic_increment(&mnRefCount); }
+
+void Adapter::release()
+{
+    if (!osl_atomic_decrement(&mnRefCount))
+        delete this;
+}
+
+css::uno::Reference<css::uno::XInterface> Adapter::queryAdapted()
+{
+    css::uno::Reference<CoreComponentInterfaces> ret;
+
+    osl::ClearableMutexGuard guard(getWeakMutex());
+
+    if (mpWeakObject)
+    {
+        oslInterlockedCount n = osl_atomic_increment(&mpWeakObject->mnRefCount);
+
+        if (n > 1)
+        {
+            guard.clear();
+            ret = mpWeakObject;
+            osl_atomic_decrement(&mpWeakObject->mnRefCount);
+        }
+        else
+        {
+            osl_atomic_decrement(&mpWeakObject->mnRefCount);
+        }
+    }
+
+    return ret;
+}
+
+void Adapter::addReference(css::uno::Reference<css::uno::XReference> const& rRef)
+{
+    osl::MutexGuard aGuard(getWeakMutex());
+    maReferences.push_back(rRef);
+}
+
+void Adapter::removeReference(css::uno::Reference<css::uno::XReference> const& rRef)
+{
+    osl::MutexGuard aGuard(getWeakMutex());
+
+    auto it = std::find(maReferences.rbegin(), maReferences.rend(), rRef);
+
+    if (it != maReferences.rend())
+        maReferences.erase(it.base() - 1);
+}
 
 css::uno::Any CoreComponentInterfaces::queryInterface(css::uno::Type const& rType)
 {
@@ -90,15 +194,33 @@ css::uno::Sequence<OUString> CoreComponentInterfaces::getSupportedServiceNames()
 
 css::uno::Sequence<css::uno::Type> CoreComponentInterfaces::getTypes()
 {
-    css::uno::Sequence<css::uno::Type> seq(2);
+    css::uno::Sequence<css::uno::Type> seq(3);
     seq[0] = cppu::UnoType<css::lang::XTypeProvider>::get();
     seq[1] = cppu::UnoType<css::lang::XServiceInfo>::get();
+    seq[2] = cppu::UnoType<css::uno::XWeak>::get();
     return seq;
 }
 
 css::uno::Sequence<sal_Int8> CoreComponentInterfaces::getImplementationId()
 {
     return css::uno::Sequence<sal_Int8>();
+}
+
+css::uno::Reference<css::uno::XAdapter> CoreComponentInterfaces::queryAdapter()
+{
+    if (!mpAdapter)
+    {
+        osl::MutexGuard aGuard(getWeakMutex());
+
+        if (!mpAdapter)
+        {
+            Adapter* pAdapter = new Adapter(this);
+            pAdapter->acquire();
+            mpAdapter = pAdapter;
+        }
+    }
+
+    return mpAdapter;
 }
 
 namespace
@@ -129,12 +251,13 @@ void Test::testCCIServiceInfo()
 void Test::testCCITypeProvider()
 {
     CoreComponentInterfaces aCoreComponentInterfaces;
-    sal_Int32 nExpectedSeqCount = 2;
+    sal_Int32 nExpectedSeqCount = 3;
 
     css::uno::Sequence<css::uno::Type> aTypes(aCoreComponentInterfaces.getTypes());
     CPPUNIT_ASSERT_EQUAL(aTypes.getLength(), nExpectedSeqCount);
     CPPUNIT_ASSERT_EQUAL(aTypes[0], cppu::UnoType<css::lang::XTypeProvider>::get());
     CPPUNIT_ASSERT_EQUAL(aTypes[1], cppu::UnoType<css::lang::XServiceInfo>::get());
+    CPPUNIT_ASSERT_EQUAL(aTypes[2], cppu::UnoType<css::uno::XWeak>::get());
 
     css::uno::Sequence<OUString> aServiceNames(aCoreComponentInterfaces.getSupportedServiceNames());
     CPPUNIT_ASSERT_EQUAL(aServiceNames[0],
