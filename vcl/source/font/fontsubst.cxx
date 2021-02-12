LibreOffice/core

vcl: move font substitution functions out of OutputDevice
Change-Id: I800702fd9a0686ecca37e64f39bd275715f2711a

vcl/source/font/fontsubst.cxx
@@ -1,0 +1,105 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <vcl/event.hxx>
+#include <vcl/outdev.hxx>
+
+#include <window.h>
+#include <font/fontsubst.hxx>
+#include <font/ImplDirectFontSubstitution.hxx>
+#include <font/ImplFontSubstEntry.hxx>
+#include <impfontcache.hxx>
+#include <salgdi.hxx>
+#include <svdata.hxx>
+#include <PhysicalFontCollection.hxx>
+
+void BeginFontSubstitution()
+{
+    ImplSVData* pSVData = ImplGetSVData();
+    pSVData->maGDIData.mbFontSubChanged = false;
+}
+
+void EndFontSubstitution()
+{
+    ImplSVData* pSVData = ImplGetSVData();
+    if (pSVData->maGDIData.mbFontSubChanged)
+    {
+        ImplUpdateAllFontData(false);
+
+        DataChangedEvent aDCEvt(DataChangedEventType::FONTSUBSTITUTION);
+        Application::ImplCallEventListenersApplicationDataChanged(&aDCEvt);
+        Application::NotifyAllWindows(aDCEvt);
+        pSVData->maGDIData.mbFontSubChanged = false;
+    }
+}
+
+void AddFontSubstitute(const OUString& rFontName, const OUString& rReplaceFontName,
+                       AddFontSubstituteFlags nFlags)
+{
+    ImplDirectFontSubstitution*& rpSubst = ImplGetSVData()->maGDIData.mpDirectFontSubst;
+    if (!rpSubst)
+        rpSubst = new ImplDirectFontSubstitution;
+    rpSubst->AddFontSubstitute(rFontName, rReplaceFontName, nFlags);
+    ImplGetSVData()->maGDIData.mbFontSubChanged = true;
+}
+
+void RemoveFontsSubstitute()
+{
+    ImplDirectFontSubstitution* pSubst = ImplGetSVData()->maGDIData.mpDirectFontSubst;
+    if (pSubst)
+        pSubst->RemoveFontsSubstitute();
+}
+
+void ImplUpdateAllFontData(bool bNewFontLists)
+{
+    ImplClearAllFontData(bNewFontLists);
+    ImplRefreshAllFontData(bNewFontLists);
+}
+
+void ImplClearAllFontData(bool bNewFontLists)
+{
+    ImplSVData* pSVData = ImplGetSVData();
+
+    UpdateFontDataForAllFrames(&OutputDevice::ImplClearFontData, bNewFontLists);
+
+    // clear global font lists to have them updated
+    pSVData->maGDIData.mxScreenFontCache->Invalidate();
+    if (!bNewFontLists)
+        return;
+
+    pSVData->maGDIData.mxScreenFontList->Clear();
+    vcl::Window* pFrame = pSVData->maFrameData.mpFirstFrame;
+    if (pFrame)
+    {
+        if (pFrame->AcquireGraphics())
+        {
+            OutputDevice* pDevice = pFrame;
+            pDevice->mpGraphics->ClearDevFontCache();
+            pDevice->mpGraphics->GetDevFontList(
+                pFrame->mpWindowImpl->mpFrameData->mxFontCollection.get());
+        }
+    }
+}
+
+void ImplRefreshAllFontData(bool bNewFontLists)
+{
+    UpdateFontDataForAllFrames(&OutputDevice::RefreshFontData, bNewFontLists);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

vcl/source/gdi/embeddedfontshelper.cxx
@@ -18,6 +18,7 @@
 #include <vcl/svapp.hxx>
 #include <vcl/virdev.hxx>
 
+#include <font/fontsubst.hxx>
 #include <font/ImplDeviceFontList.hxx>
 #include <salgdi.hxx>
 #include <sft.hxx>
@@ -169,12 +170,12 @@ namespace
     {
         UpdateFontsGuard()
         {
-            OutputDevice::ImplClearAllFontData(true);
+            ImplClearAllFontData(true);
         }
 
         ~UpdateFontsGuard()
         {
-            OutputDevice::ImplRefreshAllFontData(true);
+            ImplRefreshAllFontData(true);
         }
     };
 }

vcl/source/window/winproc.cxx
@@ -45,6 +45,7 @@
 #include <vcl/uitest/logger.hxx>
 #include <vcl/ptrstyle.hxx>
 
+#include <font/fontsubst.hxx>
 #include <svdata.hxx>
 #include <salwtype.hxx>
 #include <salframe.hxx>
@@ -2183,7 +2184,7 @@ static void ImplHandleSalSettings( SalEvent nEvent )
                 nType = DataChangedEventType::DISPLAY;
                 break;
             case SalEvent::FontChanged:
-                OutputDevice::ImplUpdateAllFontData( true );
+                ImplUpdateAllFontData( true );
                 nType = DataChangedEventType::FONTS;
                 break;
             default:

chrissherlock/libreoffice-experimental

vcl/source/font/fontsubst.cxx
@@ -1,0 +1,105 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <vcl/event.hxx>
+#include <vcl/fontsubst.hxx>
+#include <vcl/outdev.hxx>
+
+#include <window.h>
+#include <font/ImplDirectFontSubstitution.hxx>
+#include <font/ImplFontSubstEntry.hxx>
+#include <impfontcache.hxx>
+#include <salgdi.hxx>
+#include <svdata.hxx>
+#include <PhysicalFontCollection.hxx>
+
+void BeginFontSubstitution()
+{
+    ImplSVData* pSVData = ImplGetSVData();
+    pSVData->maGDIData.mbFontSubChanged = false;
+}
+
+void EndFontSubstitution()
+{
+    ImplSVData* pSVData = ImplGetSVData();
+    if (pSVData->maGDIData.mbFontSubChanged)
+    {
+        ImplUpdateAllFontData(false);
+
+        DataChangedEvent aDCEvt(DataChangedEventType::FONTSUBSTITUTION);
+        Application::ImplCallEventListenersApplicationDataChanged(&aDCEvt);
+        Application::NotifyAllWindows(aDCEvt);
+        pSVData->maGDIData.mbFontSubChanged = false;
+    }
+}
+
+void AddFontSubstitute(const OUString& rFontName, const OUString& rReplaceFontName,
+                       AddFontSubstituteFlags nFlags)
+{
+    ImplDirectFontSubstitution*& rpSubst = ImplGetSVData()->maGDIData.mpDirectFontSubst;
+    if (!rpSubst)
+        rpSubst = new ImplDirectFontSubstitution;
+    rpSubst->AddFontSubstitute(rFontName, rReplaceFontName, nFlags);
+    ImplGetSVData()->maGDIData.mbFontSubChanged = true;
+}
+
+void RemoveFontsSubstitute()
+{
+    ImplDirectFontSubstitution* pSubst = ImplGetSVData()->maGDIData.mpDirectFontSubst;
+    if (pSubst)
+        pSubst->RemoveFontsSubstitute();
+}
+
+void ImplUpdateAllFontData(bool bNewFontLists)
+{
+    ImplClearAllFontData(bNewFontLists);
+    ImplRefreshAllFontData(bNewFontLists);
+}
+
+void ImplClearAllFontData(bool bNewFontLists)
+{
+    ImplSVData* pSVData = ImplGetSVData();
+
+    UpdateFontDataForAllFrames(&OutputDevice::ImplClearFontData, bNewFontLists);
+
+    // clear global font lists to have them updated
+    pSVData->maGDIData.mxScreenFontCache->Invalidate();
+    if (!bNewFontLists)
+        return;
+
+    pSVData->maGDIData.mxScreenFontList->Clear();
+    vcl::Window* pFrame = pSVData->maFrameData.mpFirstFrame;
+    if (pFrame)
+    {
+        if (pFrame->AcquireGraphics())
+        {
+            OutputDevice* pDevice = pFrame;
+            pDevice->mpGraphics->ClearDevFontCache();
+            pDevice->mpGraphics->GetDevFontList(
+                pFrame->mpWindowImpl->mpFrameData->mxFontCollection.get());
+        }
+    }
+}
+
+void ImplRefreshAllFontData(bool bNewFontLists)
+{
+    UpdateFontDataForAllFrames(&OutputDevice::RefreshFontData, bNewFontLists);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

vcl/source/gdi/embeddedfontshelper.cxx
@@ -15,6 +15,7 @@
 #include <rtl/bootstrap.hxx>
 
 #include <vcl/embeddedfontshelper.hxx>
+#include <vcl/fontsubst.hxx>
 #include <vcl/svapp.hxx>
 #include <vcl/virdev.hxx>
 
@@ -169,12 +170,12 @@ namespace
     {
         UpdateFontsGuard()
         {
-            OutputDevice::ImplClearAllFontData(true);
+            ImplClearAllFontData(true);
         }
 
         ~UpdateFontsGuard()
         {
-            OutputDevice::ImplRefreshAllFontData(true);
+            ImplRefreshAllFontData(true);
         }
     };
 }

vcl/source/window/winproc.cxx
@@ -30,6 +30,7 @@
 #include <vcl/QueueInfo.hxx>
 #include <vcl/timer.hxx>
 #include <vcl/event.hxx>
+#include <vcl/fontsubst.hxx>
 #include <vcl/GestureEvent.hxx>
 #include <vcl/settings.hxx>
 #include <vcl/svapp.hxx>
@@ -2177,7 +2178,7 @@ static void ImplHandleSalSettings( SalEvent nEvent )
                 nType = DataChangedEventType::DISPLAY;
                 break;
             case SalEvent::FontChanged:
-                OutputDevice::ImplUpdateAllFontData( true );
+                ImplUpdateAllFontData( true );
                 nType = DataChangedEventType::FONTS;
                 break;
             default: