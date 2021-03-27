8155d4fcc8a484c6462afe339321302336746deb
Chris Sherlock <chris.sherlock79@gmail.com>
2021-03-27 14:01:14 +1100

    tdf#74702: remove GetOutDevType() from Animation::Start()
    Change-Id: I8876ea73e19686b0d1799966c5128d2764715ef0

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -584,6 +584,8 @@ public:
     void DrawImage(Point const& rPos, Size const& rSize, Image const& rImage,
                    DrawImageFlags nStyle = DrawImageFlags::NONE) override;
 
+    virtual bool CanAnimate();
+
     void DrawTransformedBitmapEx(const basegfx::B2DHomMatrix& rTransformation,
                                  const BitmapEx& rBitmapEx, double fAlpha = 1.0) override;
 
