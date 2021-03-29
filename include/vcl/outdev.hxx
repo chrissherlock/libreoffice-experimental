8e1fcba76ab5d48e2c4369b305e57192bb942d76
Chris Sherlock <chris.sherlock79@gmail.com>
2021-03-30 00:52:08 +1100

    tdf#124176 vcl: Use pragma once instead of include guards in outdev.hxx
    Change-Id: I4b2da3303f16510b540b608299539802c2b7d037

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_OUTDEV_HXX
-#define INCLUDED_VCL_OUTDEV_HXX
+#pragma once
 
 #include <vcl/RenderContext2.hxx>
 #include <vcl/cairo.hxx>
@@ -578,7 +577,5 @@ private:
     const OutDevType meOutDevType;
     OutDevViewType meOutDevViewType;
 };
-
-#endif // INCLUDED_VCL_OUTDEV_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
