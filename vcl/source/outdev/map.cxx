0178321f1ce58aaa474c7a59e9d734b206d7dc19
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-12-27 13:27:04 +1100

    vcl: rename opaque ENTER4 function in map.cxx
    The function ENTER4 in vcl/source/outdev/map.cxx has a name that gives
    no indication of its purpose, making the code difficult to read.
    
    This function calculates the map resolutions for a source and destination
    MapMode using a standardized 72 DPI, which is necessary for logical unit
    conversions.
    
    Rename it to lcl_calcConversionMapRes to clearly reflect its purpose
    and improve maintainability.
    
    Change-Id: I2cc265c07c4108a3a3a37356a148ae9ea37a3b9b

--- vcl/source/outdev/map.cxx
+++ vcl/source/outdev/map.cxx
@@ -1420,7 +1420,7 @@ auto lcl_getCorrectedUnit(MapUnit eMapSrc, MapUnit eMapDst)
     return std::make_pair(eSrc, eDst);
 }
 
-std::pair<ImplMapRes, ImplMapRes> ENTER4(const MapMode& rMMSource, const MapMode& rMMDest)
+std::pair<ImplMapRes, ImplMapRes> lcl_calcConversionMapRes(const MapMode& rMMSource, const MapMode& rMMDest)
 {
     std::pair<ImplMapRes, ImplMapRes> result;
     lcl_calcMapResolution(rMMSource, 72, 72, result.first);
@@ -1687,7 +1687,7 @@ Point OutputDevice::LogicToLogic( const Point& rPtSource,
     }
     else
     {
-        const auto [aMapResSource, aMapResDest] = ENTER4( rMapModeSource, rMapModeDest );
+        const auto [aMapResSource, aMapResDest] = lcl_calcConversionMapRes( rMapModeSource, rMapModeDest );
 
         return Point( lcl_scaleLogicValue( rPtSource.X() + aMapResSource.mnMapOfsX,
                            aMapResSource.mnMapScNumX, aMapResDest.mnMapScDenomX,
@@ -1718,7 +1718,7 @@ Size OutputDevice::LogicToLogic( const Size& rSzSource,
     }
     else
     {
-        const auto [aMapResSource, aMapResDest] = ENTER4( rMapModeSource, rMapModeDest );
+        const auto [aMapResSource, aMapResDest] = lcl_calcConversionMapRes( rMapModeSource, rMapModeDest );
 
         return Size( lcl_scaleLogicValue( rSzSource.Width(),
                           aMapResSource.mnMapScNumX, aMapResDest.mnMapScDenomX,
@@ -1769,7 +1769,7 @@ basegfx::B2DHomMatrix OutputDevice::LogicToLogic(const MapMode& rMapModeSource, 
     }
     else
     {
-        const auto [aMapResSource, aMapResDest] = ENTER4(rMapModeSource, rMapModeDest);
+        const auto [aMapResSource, aMapResDest] = lcl_calcConversionMapRes(rMapModeSource, rMapModeDest);
 
         const double fScaleFactorX((double(aMapResSource.mnMapScNumX) * double(aMapResDest.mnMapScDenomX)) / (double(aMapResSource.mnMapScDenomX) * double(aMapResDest.mnMapScNumX)));
         const double fScaleFactorY((double(aMapResSource.mnMapScNumY) * double(aMapResDest.mnMapScDenomY)) / (double(aMapResSource.mnMapScDenomY) * double(aMapResDest.mnMapScNumY)));
@@ -1813,7 +1813,7 @@ tools::Rectangle OutputDevice::LogicToLogic( const tools::Rectangle& rRectSource
     }
     else
     {
-        const auto [aMapResSource, aMapResDest] = ENTER4( rMapModeSource, rMapModeDest );
+        const auto [aMapResSource, aMapResDest] = lcl_calcConversionMapRes( rMapModeSource, rMapModeDest );
 
         auto left = lcl_scaleLogicValue( rRectSource.Left() + aMapResSource.mnMapOfsX,
                                aMapResSource.mnMapScNumX, aMapResDest.mnMapScDenomX,
