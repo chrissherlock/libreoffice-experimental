c527f75bf27421f1e2a69d830c77d6af7b21a416
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-12-31 23:02:27 +1100

    vcl: make OutputDevice mapping functions private
    Change-Id: I7d53932316a417f10355b675595fc28152867dbc

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -1760,6 +1760,7 @@ public:
      */
     SAL_DLLPRIVATE tools::Long         ImplDevicePixelToLogicHeight( tools::Long nHeight ) const;
 
+private:
     /** Convert a logical size to the size on the physical device.
 
      @param         rLogicSize  Const reference to a size in logical units
@@ -1828,7 +1829,6 @@ public:
      */
     SAL_DLLPRIVATE basegfx::B2DHomMatrix ImplGetDeviceTransformation() const;
 
-private:
     /** Convert a logical X coordinate to a device pixel's X coordinate.
 
      To get the device's X coordinate, it must calculate the mapping offset
