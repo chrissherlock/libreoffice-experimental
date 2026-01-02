2eeedab9c255b61582b280b83c265dc080e76b99
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-01-02 16:27:42 +1100

    vcl: declare i into for loop
    Change-Id: I5f7f46d786c2e7e34c8277acee689820c5f74305

--- vcl/source/outdev/map.cxx
+++ vcl/source/outdev/map.cxx
@@ -501,7 +501,6 @@ tools::Polygon OutputDevice::ImplLogicToDevicePixel(const tools::Polygon& rLogic
     if (!mbMap && !GetOutOffXPixel() && !GetOutOffYPixel())
         return rLogicPoly;
 
-    sal_uInt16 i;
     sal_uInt16 nPoints = rLogicPoly.GetSize();
     tools::Polygon aPoly(rLogicPoly);
 
@@ -510,7 +509,7 @@ tools::Polygon OutputDevice::ImplLogicToDevicePixel(const tools::Polygon& rLogic
 
     if (mbMap)
     {
-        for (i = 0; i < nPoints; i++)
+        for (sal_uInt16 i = 0; i < nPoints; i++)
         {
             const Point& rPt = pPointAry[i];
             Point aPt(lcl_logicToPixel(rPt.X() + maMapRes.mnMapOfsX, GetDPIX(), maMapRes.mnMapScNumX,
@@ -524,7 +523,7 @@ tools::Polygon OutputDevice::ImplLogicToDevicePixel(const tools::Polygon& rLogic
     }
     else
     {
-        for (i = 0; i < nPoints; i++)
+        for (sal_uInt16 i = 0; i < nPoints; i++)
         {
             Point aPt = pPointAry[i];
             aPt.AdjustX(GetOutOffXPixel());
