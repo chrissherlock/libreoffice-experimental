5af9ddf24087774811515a1b8fb510f8515f1cfb
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-01-09 22:25:28 +1100

    vcl: migrate Font state to GraphicsState
    Moves the maFont member variable from OutputDevice to the
    GraphicsState struct (accessed via the mpGraphicsState unique_ptr).
    
    Updates GraphicsState.hxx to include <vcl/font.hxx> to support
    the new member.
    
    Updates the following implementations to access the font via mpGraphicsState:
    - OutputDevice::GetFont
    - OutputDevice::SetFont (carefully preserving the mbNewFont/mbInitFont
      cache invalidation flags in OutputDevice)
    - OutputDevice::InitFont (passing the new location to the driver)
    - OutputDevice::Push (saving the font state from the new location)
    
    This separates the logical "requested font" state from the physical
    device management flags (which remain in OutputDevice).
    
    Change-Id: I87c34b70b449a680a3a3d9c8ca3531e0b8af1d6d

--- vcl/source/gdi/virdev.cxx
+++ vcl/source/gdi/virdev.cxx
@@ -184,7 +184,7 @@ void VirtualDevice::ImplInitVirDev( const OutputDevice* pOutDev,
     SetDPIX(pOutDev->GetDPIX());
     SetDPIY(pOutDev->GetDPIY());
     SetDPIScalePercentage(pOutDev->GetDPIScalePercentage());
-    maFont          = pOutDev->maFont;
+    mpGraphicsState->maFont          = pOutDev->mpGraphicsState->maFont;
 
     if( GetTextColor() != pOutDev->GetTextColor() )
     {
