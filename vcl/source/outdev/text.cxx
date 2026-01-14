40f2b05118a312d217ddc2d6c53ef7e2f604d416
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-01-14 21:51:46 +1100

    vcl: sync mnTextLayoutMode to FontRealization
    Change-Id: I0ea439bd7f00049e3e17ac0c8bce5fb322441df2

--- vcl/source/outdev/text.cxx
+++ vcl/source/outdev/text.cxx
@@ -65,6 +65,8 @@ void OutputDevice::SetLayoutMode( vcl::text::ComplexTextLayoutFlags nTextLayoutM
         mpMetaFile->AddAction( new MetaLayoutModeAction( nTextLayoutMode ) );
 
     mpGraphicsState->mnTextLayoutMode = nTextLayoutMode;
+    if (mpFontRealization)
+        mpFontRealization->eLayoutMode = nTextLayoutMode;
 }
 
 LanguageType OutputDevice::GetDigitLanguage() const
