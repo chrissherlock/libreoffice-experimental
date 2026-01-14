02d6991b798567081d993bc712c53f5b2eb969eb
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-01-14 19:22:48 +1100

    vcl: use FontRealization for GetEmphasisDescent()
    Change-Id: Ic2a0bd5d51a9c6e05f2d0ada07ed6dbd3438dc1b

--- vcl/source/outdev/font.cxx
+++ vcl/source/outdev/font.cxx
@@ -1298,5 +1298,7 @@ tools::Long OutputDevice::GetEmphasisAscent() const
 
 tools::Long OutputDevice::GetEmphasisDescent() const
 {
+    if (mpFontRealization)
+        return mpFontRealization->nEmphasisDescent;
     return mnEmphasisDescent;
 }
