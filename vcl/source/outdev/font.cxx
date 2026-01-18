d6e3b1d56b4c117a6b9d630c8d1ff152bf70f765
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-01-18 20:45:22 +1100

    vcl: remove unnecessary comments and newlines
    Change-Id: Ib6f4b23a5f31c83dc3c5f4dac1f325f9c2fab906

--- vcl/source/outdev/font.cxx
+++ vcl/source/outdev/font.cxx
@@ -131,7 +131,6 @@ FontMetric OutputDevice::GetFontMetricFromCollection(sal_uInt32 nDevFontIndex) c
     if (!mpGraphics)
         AcquireGraphics();
 
-    // Delegate to the controller, which should handle the collection lifecycle
     return mpFontController->GetFontMetricFromCollection(mpGraphics, nDevFontIndex);
 }
 
@@ -165,31 +164,29 @@ bool OutputDevice::GetFontFeatures(std::vector<vcl::font::Feature>& rFontFeature
     if (!ImplNewFont())
         return false;
 
-    // [Step 9] Refactor: Use FontRealization
     if (mpFontRealization && mpFontRealization->mxFont)
     {
         mpFontController->GetFontFeatures(mpFontRealization->mxFont.get(), rFontFeatures);
         return true;
     }
+
     return false;
 }
 
 FontMetric OutputDevice::GetFontMetric() const
 {
     FontMetric aMetric;
+
     if (!ImplNewFont())
         return aMetric;
 
-    // Initialize with device-specific logical font and alignment
     aMetric = mpGraphicsState->maFont;
     aMetric.SetAlignment(TextAlign::ALIGN_TOP);
 
-    // Delegate the complex data mapping to the controller
     mpFontController->PopulateFontMetric(
         aMetric, mpGraphicsState->maFont, mpFontRealization->mxFont.get(),
         mpFontRealization->nEmphasisAscent, mpFontRealization->nEmphasisDescent);
 
-    // Convert metrics from pixels to logical units
     aMetric.SetFontSize(PixelToLogic(aMetric.GetFontSize()));
     aMetric.SetAscent(DevicePixelToLogicHeight(aMetric.GetAscent()));
     aMetric.SetDescent(DevicePixelToLogicHeight(aMetric.GetDescent()));
@@ -236,9 +233,9 @@ bool OutputDevice::GetFontCapabilities(vcl::FontCapabilities& rFontCapabilities)
 
 tools::Long OutputDevice::GetFontExtLeading() const
 {
-    // [Step 9] Refactor: Use FontRealization
     if (mpFontRealization && mpFontRealization->mxFont)
         return mpFontRealization->mxFont->mxFontMetric->GetExternalLeading();
+
     return 0;
 }
 
