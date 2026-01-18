20a65984ee9153ae21477b68dda6cd029b7cd1e7
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-01-18 12:25:12 +1100

    vcl: introduce a font metric scaling unit test
    Change-Id: I36307a18ed250b1d61dae6b994eba3924188f09d

--- vcl/source/outdev/font.cxx
+++ vcl/source/outdev/font.cxx
@@ -655,13 +655,9 @@ vcl::Font OutputDevice::GetDefaultFont(DefaultFontType nType, LanguageType eLang
 
 void OutputDevice::ImplInitFontList() const
 {
-    // [Refactor] Delegate to Controller
     if (mpFontController)
         mpFontController->InitializeFonts(mpGraphics);
 }
-
-
-// [Restored Functions]
 
 bool OutputDevice::InitFont() const
 {
