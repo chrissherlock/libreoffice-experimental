f613d315e177c0a014a4526c4165c7253ff03037
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-01-18 23:25:25 +1100

    vcl: ensure graphics acquisition in delegated font methods
    Adds missing AcquireGraphics() calls to AddTempDevFont,
    RemoveTempDevFont, and GetFontCapabilities.
    
    This prevents passing a null SalGraphics pointer to the
    FontController when these methods are called on a device that
    has not yet initialized its graphics backend.
    
    Change-Id: I91e017562e2139b2589e20be8f09e5c53cb17460

--- vcl/source/outdev/font.cxx
+++ vcl/source/outdev/font.cxx
@@ -135,11 +135,15 @@ bool OutputDevice::IsFontAvailable(std::u16string_view rFontName) const
 
 bool OutputDevice::AddTempDevFont(const OUString& rFileURL, const OUString& rFontName) const
 {
+    if (!mpGraphics && !const_cast<OutputDevice*>(this)->AcquireGraphics())
+        return false;
     return mpFontController && mpFontController->AddTempDevFont(mpGraphics, rFileURL, rFontName);
 }
 
 bool OutputDevice::RemoveTempDevFont(const OUString& rFileURL, const OUString& rFontName)
 {
+    if (!mpGraphics && !AcquireGraphics())
+        return false;
     return mpFontController && mpFontController->RemoveTempDevFont(mpGraphics, rFileURL, rFontName);
 }
 
@@ -218,6 +222,8 @@ bool OutputDevice::GetFontCharMap(FontCharMapRef& rxFontCharMap) const
 
 bool OutputDevice::GetFontCapabilities(vcl::FontCapabilities& rFontCapabilities) const
 {
+    if (!mpGraphics && !const_cast<OutputDevice*>(this)->AcquireGraphics())
+        return false;
     return mpFontController && mpFontController->GetFontCapabilities(mpGraphics, rFontCapabilities);
 }
 
