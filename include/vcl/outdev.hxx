40ddc801072013e05025ed1a06c238bc1f8b93f1
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-01-25 21:48:50 +1100

    vcl: remove redundant ImplInitAboveTextLineSize() wrapper
    Following the migration to TextLayoutEngine, remove the legacy
    ImplInitAboveTextLineSize() method from the OutputDevice interface.
    
    - Delete declaration from outdev.hxx
    - Delete implementation from textline.cxx
    - Update PDFWriterImpl to call the LayoutEngine directly using
      public accessors
    
    This completes the transition of text line metric initialization
    to the centralized engine.
    
    Change-Id: Ia656a8888e0a32985cf979d6d09a9402f5afb92f

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -1147,7 +1147,6 @@ public:
                                                       const sal_Int32 nEndIndex ) const;
 
 protected:
-    SAL_DLLPRIVATE void         ImplInitAboveTextLineSize();
     SAL_DLLPRIVATE float        approximate_char_width() const;
 
     virtual bool shouldDrawWavePixelAsRect(tools::Long nLineWidth) const;
