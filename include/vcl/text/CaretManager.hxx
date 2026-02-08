b42a78c3fc78209097ef8fe177304ca18ac7964c
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-02-08 23:34:13 +1100

    vcl: register CppunitTest_vcl_text_caret_manager in Module_vcl
    Enables the new unit test suite for the CaretManager component. This is
    part of the ongoing effort to extract caret and cursor positioning logic
    from the monolithic TextLayoutEngine into a dedicated, testable
    CaretManager class.
    
    Registering the test in gb_Module_add_check_targets ensures it is
    executed during 'make check' and 'make vcl.check', providing validation
    for BiDi caret calculations, mirroring, and coordinate conversions.
    
    Change-Id: I2e8ac492a5c0dede0582b47be5239a1dc323472c

---
+++ include/vcl/text/CaretManager.hxx
@@ -0,0 +1,25 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <vcl/dllapi.h>
+#include <vcl/kernarray.hxx>
+#include <tools/gen.hxx>
+#include <vector>
+
+namespace vcl::text
+{
+class VCL_DLLPUBLIC CaretManager
+{
+public:
+    // We will migrate these one by one
+};
+
+} // namespace vcl::text
--- vcl/Module_vcl.mk
+++ vcl/Module_vcl.mk
@@ -268,6 +268,7 @@ $(eval $(call gb_Module_add_check_targets,vcl,\
     CppunitTest_vcl_text_recording \
     CppunitTest_vcl_text_analyzer \
     CppunitTest_vcl_text_geometry \
+    CppunitTest_vcl_text_caret_manager \
 ))
 
 ifeq ($(USING_X11),TRUE)
