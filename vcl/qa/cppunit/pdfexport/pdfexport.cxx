166fb618b1e534efc56166b6eff1865427b2f482
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-02-13 18:55:09 +1100

    vcl: relax font name check in PdfExportTest::testTdf66597_2
    The unit test testTdf66597_2 was failing on environments using newer
    versions of HarfBuzz (e.g., version 26.0.0 on macOS). The font sub-system
    is now returning the full PostScript name (including the style suffix)
    rather than just the family name.
    
    Failure log:
      equality assertion failed
      - Expected: ReemKufi
      - Actual  : ReemKufi-Regular
    
    Change the strict equality check to match() (prefix check) to allow
    the test to pass whether the suffix is present or not, as "ReemKufi-Regular"
    is a valid identification for this font.
    
    Change-Id: I8b6c2991dfac778c4dcf299b48a4db4f81040e99

--- vcl/qa/cppunit/pdfexport/pdfexport.cxx
+++ vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -1866,7 +1866,7 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf66597_2)
                                            pName);
                     fontName = pName->GetValue();
                 }
-                CPPUNIT_ASSERT_EQUAL_MESSAGE("Unexpected font name", "ReemKufi"_ostr, fontName);
+                CPPUNIT_ASSERT_MESSAGE("Unexpected font name", fontName.match("ReemKufi"_ostr);
 
                 auto pToUnicodeRef = dynamic_cast<vcl::filter::PDFReferenceElement*>(
                     pObject->Lookup("ToUnicode"_ostr));
