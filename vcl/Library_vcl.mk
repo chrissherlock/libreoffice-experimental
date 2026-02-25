c842fadff9dfddf0db244334c025fe24d79cd0f0
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-02-26 01:02:24 +1100

    vcl: move EmphasisMarks.cxx from outdev to font
    Change-Id: Icb044a953ce55e55906a9f9d82ef64bf196d4092

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -293,7 +293,6 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/outdev/background \
     vcl/source/outdev/eps \
     vcl/source/outdev/outdev \
-    vcl/source/outdev/EmphasisMarks \
     vcl/source/outdev/stack \
     vcl/source/outdev/clipping \
     vcl/source/outdev/fill \
@@ -574,6 +573,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/filter/webp/writer \
     vcl/source/font/DirectFontSubstitution \
     vcl/source/font/EmphasisMark \
+    vcl/source/font/EmphasisMarks \
     vcl/source/font/Feature \
     vcl/source/font/FeatureCollector \
     vcl/source/font/FeatureParser \
