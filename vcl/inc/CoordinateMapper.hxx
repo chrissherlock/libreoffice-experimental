2d539d122926db0dd42cdd0037e1f904e16ab765
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-04-11 21:29:57 +1000

    vcl: rename SetOutOff{X|Y}Pixel to SetDeviceOrigin{X|Y}
    These function names are incredibly confusing. SetOutOffXPixel sounds
    way too much like SetPixelOffset, but they control completely different
    things. SetPixelOffset handles the internal content scrolling, while
    SetOutOffXPixel actually sets the window's position on the backing
    system.
    
    I initially expected LogicToPixel to account for "Output Offsets"
    because of the name, but that is incorrect (it's only used in
    LogicToDevicePixel). The old names just leaked the internal Hungarian
    notation (mnOutOffX) rather than describing what the function actually
    does.
    
    Renaming these to SetDeviceOrigin makes it clear this affects the
    device coordinate space and helps distinguish it from the logical view
    offset.
    
    Change-Id: I3f480f15e19eb79121f7eb037facfc06819faa23

--- vcl/inc/CoordinateMapper.hxx
+++ vcl/inc/CoordinateMapper.hxx
@@ -68,7 +68,9 @@ public:
     float GetDPIScaleFactor() const;
     void SetDPIScalePercentage(sal_Int32 nPercentage);
 
-    tools::Long GetOutOffXPixel() const;
+    float GetDPIScaleFactor() const;
+
+    tools::Long GetDeviceOriginX() const;
     tools::Long GetOutOffYPixel() const;
 
     void SetOutOffXPixel(tools::Long nOutOffX);
