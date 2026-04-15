551473b6d9a9addea245dca15fcebb8ed153dd8a
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-04-16 01:35:33 +1000

    vcl: unify Region transformations in outdev map.cxx
    This commit eliminates duplicated routing logic between
    OutputDevice::LogicToPixel(const vcl::Region&) and
    OutputDevice::PixelToLogic(const vcl::Region&).
    
    The identical data-structure traversal (handling B2DPolyPolygon,
    PolyPolygon, and RegionBand) has been extracted into a generic
    template helper lcl_TransformRegion().
    
    Technical details:
    - Used a C++14 generic lambda (const auto& obj) to let the compiler
      automatically resolve the correct LogicToPixel/PixelToLogic overload.
    - Removed a legacy C++98 const_reverse_iterator workaround during
      the RegionBand RectangleVector traversal, taking advantage of modern
      auto type deduction.
    
    Change-Id: Ie77fafd86c5024b56f1a1d99ec7ae9dc91408518

--- vcl/source/outdev/map.cxx
+++ vcl/source/outdev/map.cxx
@@ -143,7 +143,7 @@ tools::Rectangle OutputDevice::LogicToDevicePixel(const tools::Rectangle& rLogic
 
 tools::Polygon OutputDevice::ImplLogicToDevicePixel( const tools::Polygon& rLogicPoly ) const
 {
-    if ( !mpMapper->IsMapModeEnabled() && !mpMapper->GetDeviceOriginX() && !mpMapper->GetDeviceOriginY() )
+    if (!mpMapper->IsMapModeEnabled() && !mpMapper->GetDeviceToWindowOffsetX() && !mpMapper->GetDeviceToWindowOffsetY())
         return rLogicPoly;
 
     tools::Polygon aPoly(rLogicPoly);
@@ -197,7 +197,7 @@ basegfx::B2DPolygon OutputDevice::ImplLogicToDevicePixel(const basegfx::B2DPolyg
 
 tools::PolyPolygon OutputDevice::ImplLogicToDevicePixel( const tools::PolyPolygon& rLogicPolyPoly ) const
 {
-    if ( !mpMapper->IsMapModeEnabled() && !mpMapper->GetDeviceOriginX() && !mpMapper->GetDeviceOriginY() )
+    if (!mpMapper->IsMapModeEnabled() && !mpMapper->GetDeviceToWindowOffsetX() && !mpMapper->GetDeviceToWindowOffsetY())
         return rLogicPolyPoly;
 
     tools::PolyPolygon aPolyPoly(rLogicPolyPoly);
@@ -278,17 +278,17 @@ void OutputDevice::SetOutputHeightPixel(tools::Long nHeight) { mpMapper->SetOutp
 
 Size OutputDevice::GetOutputSizePixel() const { return Size(GetOutputWidthPixel(), GetOutputHeightPixel()); }
 
-tools::Long OutputDevice::GetDeviceOriginX() const { return mpMapper->GetDeviceOriginX(); }
+tools::Long OutputDevice::GetDeviceOriginX() const { return mpMapper->GetDeviceToWindowOffsetX(); }
 
-tools::Long OutputDevice::GetDeviceOriginY() const { return mpMapper->GetDeviceOriginY(); }
+tools::Long OutputDevice::GetDeviceOriginY() const { return mpMapper->GetDeviceToWindowOffsetY(); }
 
-void OutputDevice::SetDeviceOriginX(tools::Long nOutOffX) { return mpMapper->SetDeviceOriginX(nOutOffX); }
+void OutputDevice::SetDeviceOriginX(tools::Long nOutOffX) { return mpMapper->SetDeviceToWindowOffsetX(nOutOffX); }
 
-void OutputDevice::SetDeviceOriginY(tools::Long nOutOffY) { return mpMapper->SetOutOffYPixel(nOutOffY); }
+void OutputDevice::SetDeviceOriginY(tools::Long nOutOffY) { return mpMapper->SetDeviceToWindowOffsetY(nOutOffY); }
 
-Point OutputDevice::GetOutputOffPixel() const { return mpMapper->GetOutputOffPixel(); }
+Point OutputDevice::GetOutputOffPixel() const { return mpMapper->GetDeviceToWindowOffset(); }
 
-Size OutputDevice::GetPixelOffset() const { return mpMapper->GetPixelOffset(); }
+Size OutputDevice::GetPixelOffset() const { return mpMapper->GetWindowToViewOffset(); }
 
 bool OutputDevice::IsMapModeEnabled() const { return mpMapper->IsMapModeEnabled(); }
 
@@ -313,7 +313,7 @@ void OutputDevice::SetMapMode()
     ImplInitMapModeObjects();
 
     // #106426# Adapt logical offset when changing mapmode
-    mpMapper->SetLogicalOffset(Size(mpMapper->GetPixelXOffset(), mpMapper->GetPixelYOffset()));
+    mpMapper->SetLogicToAbsoluteOffset(mpMapper->GetWindowToViewOffset());
 
     // #i75163#
     mpMapper->InvalidateViewTransform();
@@ -385,8 +385,8 @@ void OutputDevice::SetMapMode( const MapMode& rNewMapMode )
     ImplInitMapModeObjects();
 
     // #106426# Adapt logical offset when changing mapmode
-    mpMapper->SetLogicalOffset(Size(mpMapper->ViewToLogicDistanceX(mpMapper->GetPixelXOffset()),
-                                    mpMapper->ViewToLogicDistanceY(mpMapper->GetPixelYOffset())));
+    mpMapper->SetLogicToAbsoluteOffset(Size(mpMapper->ViewToLogicDistanceX(mpMapper->GetWindowToViewOffsetX()),
+                                    mpMapper->ViewToLogicDistanceY(mpMapper->GetWindowToViewOffsetY())));
 
     // #i75163#
     mpMapper->InvalidateViewTransform();
@@ -455,19 +455,18 @@ void OutputDevice::SetRelativeMapMode( const MapMode& rNewMapMode )
         mpMapper->ResetMapMode(rNewMapMode);
 
     // #106426# Adapt logical offset when changing MapMode
-    mpMapper->SetLogicalOffset(Size(mpMapper->ViewToLogicDistanceX(mpMapper->GetPixelXOffset()),
-                                    mpMapper->ViewToLogicDistanceY(mpMapper->GetPixelYOffset())));
+    mpMapper->SetLogicToAbsoluteOffset(Size(mpMapper->ViewToLogicDistanceX(mpMapper->GetWindowToViewOffsetX()),
+                                    mpMapper->ViewToLogicDistanceY(mpMapper->GetWindowToViewOffsetY())));
 }
 
 Point OutputDevice::LogicToPixel( const Point& rLogicPt ) const
 {
-
     if ( !mpMapper->IsMapModeEnabled() )
         return rLogicPt;
 
     return Point(
-        mpMapper->ViewToWindowUnitsX(mpMapper->LogicUnitsToViewUnitsX(rLogicPt.X())),
-        mpMapper->ViewToWindowUnitsY(mpMapper->LogicUnitsToViewUnitsY(rLogicPt.Y()))
+        mpMapper->LogicToWindowUnitsX(rLogicPt.X()),
+        mpMapper->LogicToWindowUnitsY(rLogicPt.Y())
     );
 }
 
@@ -538,44 +537,54 @@ basegfx::B2DPolyPolygon OutputDevice::LogicToPixel( const basegfx::B2DPolyPolygo
     return aTransformedPoly;
 }
 
-vcl::Region OutputDevice::LogicToPixel( const vcl::Region& rLogicRegion ) const
+template<typename TransformFunc>
+static vcl::Region lcl_TransformRegion(const vcl::Region& rRegion, TransformFunc&& func)
 {
-
-    if(!mpMapper->IsMapModeEnabled() || rLogicRegion.IsNull() || rLogicRegion.IsEmpty())
-    {
-        return rLogicRegion;
-    }
+    if (rRegion.IsNull() || rRegion.IsEmpty())
+        return rRegion;
 
     vcl::Region aRegion;
-
-    if(rLogicRegion.getB2DPolyPolygon())
+    if (rRegion.getB2DPolyPolygon())
     {
-        aRegion = vcl::Region(LogicToPixel(*rLogicRegion.getB2DPolyPolygon()));
+        aRegion = vcl::Region(func(*rRegion.getB2DPolyPolygon()));
     }
-    else if(rLogicRegion.getPolyPolygon())
+    else if (rRegion.getPolyPolygon())
     {
-        aRegion = vcl::Region(LogicToPixel(*rLogicRegion.getPolyPolygon()));
+        aRegion = vcl::Region(func(*rRegion.getPolyPolygon()));
     }
-    else if(rLogicRegion.getRegionBand())
+    else if (rRegion.getRegionBand())
     {
         RectangleVector aRectangles;
-        rLogicRegion.GetRegionRectangles(aRectangles);
-        const RectangleVector& rRectangles(aRectangles); // needed to make the '!=' work
+        rRegion.GetRegionRectangles(aRectangles);
 
-        // make reverse run to fill new region bottom-up, this will speed it up due to the used data structuring
-        for(RectangleVector::const_reverse_iterator aRectIter(rRectangles.rbegin()); aRectIter != rRectangles.rend(); ++aRectIter)
+        // Reverse run to fill new region bottom-up for speed
+        for (auto aRectIter = aRectangles.rbegin(); aRectIter != aRectangles.rend(); ++aRectIter)
         {
-            aRegion.Union(LogicToPixel(*aRectIter));
+            aRegion.Union(func(*aRectIter));
         }
     }
-
     return aRegion;
+}
+
+vcl::Region OutputDevice::LogicToPixel(const vcl::Region& rLogicRegion) const
+{
+    if (!mpMapper->IsMapModeEnabled())
+        return rLogicRegion;
+
+    return lcl_TransformRegion(rLogicRegion, [this](const auto& obj) { return LogicToPixel(obj); });
+}
+
+vcl::Region OutputDevice::PixelToLogic(const vcl::Region& rDeviceRegion) const
+{
+    if (!mpMapper->IsMapModeEnabled())
+        return rDeviceRegion;
+
+    return lcl_TransformRegion(rDeviceRegion, [this](const auto& obj) { return PixelToLogic(obj); });
 }
 
 Point OutputDevice::LogicToPixel( const Point& rLogicPt,
                                   const MapMode& rMapMode ) const
 {
-
     if ( rMapMode.IsDefault() )
         return rLogicPt;
 
@@ -583,8 +592,8 @@ Point OutputDevice::LogicToPixel( const Point& rLogicPt,
     ImplMapRes aMapRes(rMapMode, mpMapper->GetDPIX(), mpMapper->GetDPIY());
 
     return Point(
-        mpMapper->ViewToWindowUnitsX(mpMapper->LogicUnitsToViewUnitsX(rLogicPt.X(), aMapRes)),
-        mpMapper->ViewToWindowUnitsY(mpMapper->LogicUnitsToViewUnitsY(rLogicPt.Y(), aMapRes))
+        mpMapper->LogicToWindowUnitsX(rLogicPt.X(), aMapRes),
+        mpMapper->LogicToWindowUnitsY(rLogicPt.Y(), aMapRes)
     );
 }
 
@@ -752,40 +761,6 @@ basegfx::B2DRectangle OutputDevice::PixelToLogic(const basegfx::B2DRectangle& rD
     const basegfx::B2DHomMatrix aTransformationMatrix = mpMapper->GetInverseViewTransformation();
     aTransformedRect.transform(aTransformationMatrix);
     return aTransformedRect;
-}
-
-vcl::Region OutputDevice::PixelToLogic( const vcl::Region& rDeviceRegion ) const
-{
-
-    if(!mpMapper->IsMapModeEnabled() || rDeviceRegion.IsNull() || rDeviceRegion.IsEmpty())
-    {
-        return rDeviceRegion;
-    }
-
-    vcl::Region aRegion;
-
-    if(rDeviceRegion.getB2DPolyPolygon())
-    {
-        aRegion = vcl::Region(PixelToLogic(*rDeviceRegion.getB2DPolyPolygon()));
-    }
-    else if(rDeviceRegion.getPolyPolygon())
-    {
-        aRegion = vcl::Region(PixelToLogic(*rDeviceRegion.getPolyPolygon()));
-    }
-    else if(rDeviceRegion.getRegionBand())
-    {
-        RectangleVector aRectangles;
-        rDeviceRegion.GetRegionRectangles(aRectangles);
-        const RectangleVector& rRectangles(aRectangles); // needed to make the '!=' work
-
-        // make reverse run to fill new region bottom-up, this will speed it up due to the used data structuring
-        for(RectangleVector::const_reverse_iterator aRectIter(rRectangles.rbegin()); aRectIter != rRectangles.rend(); ++aRectIter)
-        {
-            aRegion.Union(PixelToLogic(*aRectIter));
-        }
-    }
-
-    return aRegion;
 }
 
 Point OutputDevice::PixelToLogic( const Point& rDevicePt,
@@ -1162,10 +1137,10 @@ tools::Long OutputDevice::LogicToLogic( tools::Long nLongSource,
 
 void OutputDevice::SetPixelOffset( const Size& rOffset )
 {
-    mpMapper->SetPixelOffset(rOffset);
+    mpMapper->SetWindowToViewOffset(Size(rOffset.Width(), rOffset.Height()));
 
-    mpMapper->SetLogicalOffset(Size(mpMapper->ViewToLogicDistanceX(mpMapper->GetPixelXOffset()),
-                                    mpMapper->ViewToLogicDistanceY(mpMapper->GetPixelYOffset())));
+    mpMapper->SetLogicToAbsoluteOffset(Size(mpMapper->ViewToLogicDistanceX(mpMapper->GetWindowToViewOffsetX()),
+                                    mpMapper->ViewToLogicDistanceY(mpMapper->GetWindowToViewOffsetY())));
 }
 
 double OutputDevice::LogicWidthToDeviceSubPixel(tools::Long nWidth) const
