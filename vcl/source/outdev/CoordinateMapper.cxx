e3b8529d615e2182559c917baca3515cdf83a138
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-04-19 14:47:57 +1000

    vcl: unify basegfx PixelToLogic mappings using C++20 concepts
    This commit leverages C++20 concepts to unify the inverse coordinate
    transformations for basegfx geometry, replacing redundant methods with
    a single, type-safe template.
    
    Key changes:
    - Defined the TransformableB2DGeometry concept to constrain types
      that implement a .transform(B2DHomMatrix) method.
    - Replaced the explicit B2DPolyPolygon implementation of
      CoordinateMapper::WindowToLogicUnits with a generic template,
      and removed the specific non-template declarations from the header
      to ensure correct compiler overload resolution.
    - Added explicit template instantiations for basegfx::B2DRectangle
      and basegfx::B2DPolyPolygon in the implementation file.
    - Streamlined OutputDevice::PixelToLogic for these types into pure
      passthroughs to the new mapper template.
    
    This provides a free architectural win by applying the
    IsMapModeEnabled() fast-path universally to all transformed basegfx
    geometry, skipping unnecessary identity matrix multiplications.
    
    Change-Id: Icf53f43df8fab916b090554b762e3e349615fee7

--- vcl/source/outdev/CoordinateMapper.cxx
+++ vcl/source/outdev/CoordinateMapper.cxx
@@ -18,6 +18,9 @@
  */
 
 #include <basegfx/matrix/b2dhommatrix.hxx>
+#include <basegfx/range/b2drectangle.hxx>
+#include <basegfx/polygon/b2dpolygon.hxx>
+#include <basegfx/polygon/b2dpolypolygon.hxx>
 #include <tools/gen.hxx>
 
 #include <vcl/lineinfo.hxx>
@@ -915,18 +918,22 @@ CoordinateMapper::WindowToLogicUnits(const tools::PolyPolygon& rWindowPolyPoly) 
     return aPolyPoly;
 }
 
-basegfx::B2DPolyPolygon
-CoordinateMapper::WindowToLogicUnits(const basegfx::B2DPolyPolygon& rWindowPolyPoly) const
+template <TransformableB2DGeometry T>
+T CoordinateMapper::WindowToLogicUnits(const T& rWindowGeometry) const
 {
     if (!IsMapModeEnabled())
-        return rWindowPolyPoly;
+        return rWindowGeometry;
 
-    basegfx::B2DPolyPolygon aTransformedPoly = rWindowPolyPoly;
-    const basegfx::B2DHomMatrix aTransformationMatrix
-        = GetInverseViewTransformation(); // Or however you grab the inverse matrix
-    aTransformedPoly.transform(aTransformationMatrix);
-    return aTransformedPoly;
+    T aTransformedGeometry = rWindowGeometry;
+    aTransformedGeometry.transform(GetInverseViewTransformation());
+    return aTransformedGeometry;
 }
+
+template SAL_DLLPRIVATE basegfx::B2DRectangle
+CoordinateMapper::WindowToLogicUnits<basegfx::B2DRectangle>(const basegfx::B2DRectangle&) const;
+
+template SAL_DLLPRIVATE basegfx::B2DPolyPolygon
+CoordinateMapper::WindowToLogicUnits<basegfx::B2DPolyPolygon>(const basegfx::B2DPolyPolygon&) const;
 
 Size CoordinateMapper::WindowToLogicUnits(const Size& rWindowSize) const
 {
