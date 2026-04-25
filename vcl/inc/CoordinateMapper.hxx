789b1728e1ae4ec23ea11392ca91354ddf89f3fa
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-04-26 00:25:47 +1000

    vcl: extract scalar snapshot math and streamline matrix construction
    Extracted the calculation of scaling and translation scalars into a new
    helper method, FillSnapshot(). This establishes the
    TransformSnapshotScalar as the definitive single source of truth
    before any matrix operations occur in the snapshot pipeline.
    
    Additionally, streamlined AcquireSnapshot() by constructing the
    maLogicToDevice affine matrix directly via the B2DHomMatrix
    constructor using the populated scalar values. This eliminates the
    need for piecemeal .set() and .translate() mutations, resulting
    in cleaner, more declarative matrix derivation.
    
    Change-Id: I2bed1c9d95413e887db0b17089ae4f1c65c1fa43

--- vcl/inc/CoordinateMapper.hxx
+++ vcl/inc/CoordinateMapper.hxx
@@ -553,6 +553,7 @@ public:
 
 private:
     void UpdateTransforms() const;
+    vcl::detail::TransformSnapshotScalar FillSnapshot() const;
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
