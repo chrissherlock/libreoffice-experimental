394d5c8aa21a315781da5d0a5ef0eda1d699db02
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-06-08 05:35:18 +1000

    vcl: encapsulate MappingPolicy within CoordinateMapper
    Migrate meMapPolicy state from OutputDevice to CoordinateMapper.
    
    This change continues the refactoring goal of centralizing coordinate-related
    state. OutputDevice no longer maintains mapping policy state, instead acting
    as a facade that delegates management to the CoordinateMapper. This ensures
    that cache invalidation via InvalidateViewTransform() remains coupled with
    policy updates, preventing stale state inconsistencies.
    
    Change-Id: I3576a8d9b2bb2efb3ccbeb1b2126df2def61be96

--- include/vcl/CoordinateMapper.hxx
+++ include/vcl/CoordinateMapper.hxx
@@ -134,6 +134,7 @@ class VCL_DLLPUBLIC CoordinateMapper
 private:
     vcl::CoordinateState maState;
     vcl::TransformRouter maRouter;
+    vcl::MappingPolicy meMapPolicy = vcl::MappingPolicy::IgnoreMapMode;
 
     tools::Long mnOutWidth = 0;
     tools::Long mnOutHeight = 0;
@@ -160,6 +161,9 @@ public:
             aObj *= GetDeviceToLogicMatrix(ePolicy);
         return aObj;
     }
+
+    void SetMappingPolicy(vcl::MappingPolicy ePolicy) { meMapPolicy = ePolicy; }
+    vcl::MappingPolicy GetMappingPolicy() const { return meMapPolicy; }
 
     basegfx::B2DHomMatrix GetLogicToLogicMatrix(const MapMode& rSrc, const MapMode& rDst) const;
 
