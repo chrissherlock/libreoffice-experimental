0c82ff255f93ea34197e4e301f0dc1043e3c7ca9
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-06-07 21:38:08 +1000

    vcl: rename internal helper functions in Printer.cxx to lcl_ prefix
    Rename internal static helper functions ImplInitPrnQueueList and
    queryFile to lcl_InitPrnQueueList and lcl_QueryFile respectively.
    
    This change follows the established LibreOffice code style convention
    of using the lcl_ prefix for file-local (internal linkage) helper
    functions, distinguishing them from class-member methods.
    
    No functional changes are intended.
    
    Change-Id: Ie949010bda2c782294bc8b3569a5aac1bddc5daa

--- vcl/source/printer/Printer.cxx
+++ vcl/source/printer/Printer.cxx
@@ -340,7 +340,7 @@ bool Printer::HasMirroredGraphics() const
     return false;
 }
 
-static void ImplInitPrnQueueList()
+static void lcl_InitPrnQueueList()
 {
     ImplSVData* pSVData = ImplGetSVData();
 
@@ -361,7 +361,7 @@ const std::vector<OUString>& Printer::GetPrinterQueues()
 {
     ImplSVData* pSVData = ImplGetSVData();
     if ( !pSVData->maGDIData.mpPrinterQueueList )
-        ImplInitPrnQueueList();
+        lcl_InitPrnQueueList();
     assert(pSVData->maGDIData.mpPrinterQueueList && "mpPrinterQueueList exists by now");
     return pSVData->maGDIData.mpPrinterQueueList->m_aPrinterList;
 }
@@ -371,7 +371,7 @@ const QueueInfo* Printer::GetQueueInfo(const OUString& rPrinterName, bool bStatu
     ImplSVData* pSVData = ImplGetSVData();
 
     if (!pSVData->maGDIData.mpPrinterQueueList)
-        ImplInitPrnQueueList();
+        lcl_InitPrnQueueList();
 
     if (!pSVData->maGDIData.mpPrinterQueueList)
         return nullptr;
@@ -733,7 +733,7 @@ SalPrinterQueueInfo* Printer::ImplGetQueueInfo(const OUString& rPrinterName,
 {
     ImplSVData* pSVData = ImplGetSVData();
     if (!pSVData->maGDIData.mpPrinterQueueList)
-        ImplInitPrnQueueList();
+        lcl_InitPrnQueueList();
 
     ImplPrnQueueList* pPrnList = pSVData->maGDIData.mpPrinterQueueList.get();
     if (!pPrnList || pPrnList->m_aQueueInfos.empty())
@@ -1731,7 +1731,7 @@ void Printer::PrintJob(const std::shared_ptr<vcl::PrinterController>& i_xControl
     }
 }
 
-static OUString queryFile( Printer const * pPrinter, const OUString & rJobName )
+static OUString lcl_QueryFile( Printer const * pPrinter, const OUString & rJobName )
 {
     OUString aResult;
 
@@ -1989,7 +1989,7 @@ bool Printer::PreparePrintJob(std::shared_ptr<vcl::PrinterController> xControlle
                 css::beans::PropertyValue* pJobNameVal = xController->getValue( u"JobName"_ustr );
                 if( pJobNameVal )
                     pJobNameVal->Value >>= aJobName;
-                OUString aFile = queryFile( xController->getPrinter().get(), aJobName );
+                OUString aFile = lcl_QueryFile( xController->getPrinter().get(), aJobName );
                 if( aFile.isEmpty() )
                 {
                     xController->abortJob();
@@ -2285,4 +2285,5 @@ void Printer::DrawGradientEx(OutputDevice* pOut, const tools::Rectangle& rRect, 
     aNewGradient.SetSteps(rPrinterOptions.GetReducedGradientStepCount());
     pOut->DrawGradient(rRect, aNewGradient);
 }
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
