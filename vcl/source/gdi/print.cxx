2093199849527a6f6a2f4356170741c210b7f488
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-06-07 20:16:19 +1000

    vcl: flatte Printer::ImplEndPage()
    Change-Id: I8495f907a09f19b5bd3bd209ef159dbd4f0a7900

--- vcl/source/gdi/print.cxx
+++ vcl/source/gdi/print.cxx
@@ -1586,15 +1586,15 @@ void Printer::ImplEndPage()
 
     mbInPrintPage = false;
 
-    if ( mpPrinter )
-    {
-        ReleaseGraphics();
-        mpPrinter->EndPage();
-        mbDevOutput = false;
+    if ( !mpPrinter )
+        return;
 
-        mpJobGraphics = nullptr;
-        mbNewJobSetup = false;
-    }
+    ReleaseGraphics();
+    mpPrinter->EndPage();
+    mbDevOutput = false;
+
+    mpJobGraphics = nullptr;
+    mbNewJobSetup = false;
 }
 
 void Printer::updatePrinters()
