16653c38caa2b2a705475e9184499c44b70a317b
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-06-07 21:33:37 +1000

    vcl: move ImplPrnQueueList to own source file
    Change-Id: I1cd18153d7a0f13a5170037975100b2a66d60d5a

--- vcl/source/printer/Printer.cxx
+++ vcl/source/printer/Printer.cxx
@@ -340,40 +340,6 @@ bool Printer::HasMirroredGraphics() const
     return false;
 }
 
-ImplPrnQueueList::~ImplPrnQueueList()
-{
-}
-
-void ImplPrnQueueList::Add( std::unique_ptr<SalPrinterQueueInfo> pData )
-{
-    std::unordered_map< OUString, sal_Int32 >::iterator it =
-        m_aNameToIndex.find( pData->maPrinterName );
-    if( it == m_aNameToIndex.end() )
-    {
-        m_aNameToIndex[ pData->maPrinterName ] = m_aQueueInfos.size();
-        m_aPrinterList.push_back( pData->maPrinterName );
-        m_aQueueInfos.push_back( ImplPrnQueueData() );
-        m_aQueueInfos.back().mpQueueInfo = nullptr;
-        m_aQueueInfos.back().mpSalQueueInfo = std::move(pData);
-    }
-    else // this should not happen, but ...
-    {
-        ImplPrnQueueData& rData = m_aQueueInfos[ it->second ];
-        rData.mpQueueInfo.reset();
-        rData.mpSalQueueInfo = std::move(pData);
-    }
-}
-
-ImplPrnQueueData* ImplPrnQueueList::Get( const OUString& rPrinter )
-{
-    ImplPrnQueueData* pData = nullptr;
-    std::unordered_map<OUString,sal_Int32>::iterator it =
-        m_aNameToIndex.find( rPrinter );
-    if( it != m_aNameToIndex.end() )
-        pData = &m_aQueueInfos[it->second];
-    return pData;
-}
-
 static void ImplInitPrnQueueList()
 {
     ImplSVData* pSVData = ImplGetSVData();
