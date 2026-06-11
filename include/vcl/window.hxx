a29c9c947babdf1b994ed76f4a4c953e8b4fd570
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-06-11 23:52:31 +1000

    vcl: Rename ImplUpdateSysObjClipRegion to ImplUpdateNativeObjectClipRegion
    Modernizes private window member method nomenclature by replacing the legacy
    "SysObj" (System Object) abbreviation with "NativeObject".
    
    This aligns the coordinate serialization pipeline with the modern naming
    conventions established across the recently overhauled `vcl::clipping`
    subsystem pipelines (such as `syncNativeWindow` and `gatherNativeSyncTargets`).
    
    Changes:
    * include/vcl/window.hxx: Update private SAL_DLLPRIVATE method declaration.
    * vcl/source/window/clipping.cxx: Update method definition and the internal
      dispatch call site located inside ImplSysObjClip.
    
    Change-Id: I39af31a388b3ed80e89af4623c972610b7e4860b

--- include/vcl/window.hxx
+++ include/vcl/window.hxx
@@ -614,7 +614,7 @@ private:
     SAL_DLLPRIVATE void                 ImplLogicToPoint(vcl::RenderContext const & rRenderContext, vcl::Font& rFont) const;
 
     SAL_DLLPRIVATE bool                 ImplSysObjClip( const vcl::Region* pOldRegion );
-    SAL_DLLPRIVATE void                 ImplUpdateSysObjClipRegion(vcl::Region aRegion, const vcl::Region& rWinRectRegion);
+    SAL_DLLPRIVATE void                 ImplUpdateNativeObjectClipRegion(vcl::Region aRegion, const vcl::Region& rWinRectRegion);
     SAL_DLLPRIVATE void                 ImplUpdateSysObjClip();
 
     SAL_DLLPRIVATE void                 ImplIntersectWindowClipRegion( vcl::Region& rRegion );
