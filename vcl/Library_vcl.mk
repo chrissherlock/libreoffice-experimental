f24797cda95fae9288250f57813ccfd1bb864318
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-08-19 11:50:16 +1000

    vcl: extract ImplWinData definition to its own source file
    This commit modularizes the VCL window implementation code by separating
    ImplWinData out of window.cxx into a dedicated source file.
    
    Key changes:
    - Added vcl/source/window/ImplWinData to Library_vcl.mk build sources.
    - Removed inline ImplWinData constructor and destructor definitions
      from window.cxx.
    
    This is a pure structural code organization cleanup.
    
    Change-Id: I97385dedaa1c4cfa9fe94165b5dff4243fd5b0f9

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -177,6 +177,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/printer/PrinterOptionsHelper \
     vcl/source/printer/QueueInfo \
     vcl/source/printer/SalPrinterQueueInfo \
+    vcl/source/window/ImplWinData \
     vcl/source/window/bubblewindow \
     vcl/source/window/clipping_subsystem \
     vcl/source/window/errinf \
