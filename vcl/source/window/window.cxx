e3f22ea41b72c1756f15c9e494b5f125ecf8765d
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-08-20 20:19:49 +1000

    vcl: move ImplFrameData constructor to ImplFrameData.cxx
    This commit extracts the ImplFrameData constructor from the massive
    window.cxx file and moves it into its own dedicated source file,
    ImplFrameData.cxx.
    
    This change also cleans up an awkward namespace break in window.cxx,
    where the vcl namespace had to be explicitly closed and reopened
    just to accommodate this constructor.
    
    Key changes:
    - Added vcl/source/window/ImplFrameData to Library_vcl.mk.
    - Moved the ImplFrameData constructor to ImplFrameData.cxx.
    - Added missing include headers to ImplFrameData.cxx to support
      the newly moved logic.
    
    Change-Id: I3b9ef8d1b12e26f985ba111480543513e8189e60

--- vcl/source/window/window.cxx
+++ vcl/source/window/window.cxx
@@ -648,64 +648,6 @@ Window::~Window()
     return mpWindowImpl ? mpWindowImpl->mxOutDev.get() : nullptr;
 }
 
-} /* namespace vcl */
-
-ImplFrameData::ImplFrameData( vcl::Window *pWindow )
-    : maPaintIdle( "vcl::Window maPaintIdle" ),
-      maResizeIdle( "vcl::Window maResizeIdle" )
-{
-    ImplSVData* pSVData = ImplGetSVData();
-    assert (pSVData->maFrameData.mpFirstFrame.get() != pWindow);
-    mpNextFrame        = pSVData->maFrameData.mpFirstFrame;
-    pSVData->maFrameData.mpFirstFrame = pWindow;
-    mpFirstOverlap     = nullptr;
-    mpFocusWin         = nullptr;
-    mpMouseMoveWin     = nullptr;
-    mpMouseDownWin     = nullptr;
-    mpTrackWin         = nullptr;
-    mxFontCollection   = pSVData->maGDIData.mxScreenFontList;
-    mxFontCache        = pSVData->maGDIData.mxScreenFontCache;
-    mnFocusId          = nullptr;
-    mnMouseMoveId      = nullptr;
-    mnLastMouseX       = -32767;
-    mnLastMouseY       = -32767;
-    mnBeforeLastMouseX = -32767;
-    mnBeforeLastMouseY = -32767;
-    mnFirstMouseX      = -32767;
-    mnFirstMouseY      = -32767;
-    mnLastMouseWinX    = -32767;
-    mnLastMouseWinY    = -32767;
-    mnModalMode        = 0;
-    mnMouseDownTime    = 0;
-    mnClickCount       = 0;
-    mnFirstMouseCode   = 0;
-    mnMouseCode        = 0;
-    mnMouseMode        = MouseEventModifiers::NONE;
-    mbHasFocus         = false;
-    mbInMouseMove      = false;
-    mbMouseIn          = false;
-    mbStartDragCalled  = false;
-    mbNeedSysWindow    = false;
-    mbMinimized        = false;
-    mbStartFocusState  = false;
-    mbInSysObjFocusHdl = false;
-    mbInSysObjToTopHdl = false;
-    mbSysObjFocus      = false;
-    maPaintIdle.SetPriority( TaskPriority::REPAINT );
-    maPaintIdle.SetInvokeHandler( LINK( pWindow, vcl::Window, ImplHandlePaintHdl ) );
-    maResizeIdle.SetPriority( TaskPriority::RESIZE );
-    maResizeIdle.SetInvokeHandler( LINK( pWindow, vcl::Window, ImplHandleResizeTimerHdl ) );
-    mbInternalDragGestureRecognizer = false;
-    mbDragging = false;
-    mbInBufferedPaint = false;
-    mnDPIX = 96;
-    mnDPIY = 96;
-    mnTouchPanPositionX = -1;
-    mnTouchPanPositionY = -1;
-}
-
-namespace vcl {
-
 static sal_Int32 CountDPIScaleFactor(sal_Int32 nDPI)
 {
 #ifndef MACOSX
