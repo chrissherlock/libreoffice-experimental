f4be19f197c7b5e607de71b0850be8f5aa7cfa10
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-08-23 17:49:25 +1000

    vcl: extract window visibility and show logic into window_visibility.cxx
    This commit continues the modularization of the Window class by
    extracting visibility management and show lifecycle logic out of the
    monolithic window.cxx file and into a new dedicated source file,
    window_visibility.cxx.
    
    This isolates complex state handling associated with Window::Show() and
    ImplSetReallyVisible(), including overlap window visibility propagation,
    focus conversions, native widget theme workarounds, and splash screen
    coordination. The build system has been updated to compile the new file.
    
    Change-Id: I12534ab73ecae5be46bfe6594b29d8473127f9af

---
+++ vcl/source/window/window_visibility.cxx
@@ -0,0 +1,293 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <vcl/window.hxx>
+
+#include <window.h>
+#include <clipping_window.hxx>
+#include <salframe.hxx>
+#include <svdata.hxx>
+
+namespace vcl
+{
+void Window::Show(bool bVisible, ShowFlags nFlags)
+{
+    if (!mpWindowImpl || mpWindowImpl->mbVisible == bVisible)
+        return;
+
+    VclPtr<vcl::Window> xWindow(this);
+
+    bool bRealVisibilityChanged = false;
+    mpWindowImpl->mbVisible = bVisible;
+
+    if (!bVisible)
+    {
+        ImplHideAllOverlaps();
+        if (!xWindow->mpWindowImpl)
+            return;
+
+        if (mpWindowImpl->mpBorderWindow)
+        {
+            bool bOldUpdate = mpWindowImpl->mpBorderWindow->mpWindowImpl->mbNoParentUpdate;
+            if (mpWindowImpl->mbNoParentUpdate)
+                mpWindowImpl->mpBorderWindow->mpWindowImpl->mbNoParentUpdate = true;
+            mpWindowImpl->mpBorderWindow->Show(false, nFlags);
+            mpWindowImpl->mpBorderWindow->mpWindowImpl->mbNoParentUpdate = bOldUpdate;
+        }
+        else if (mpWindowImpl->mbFrame)
+        {
+            mpWindowImpl->mbSuppressAccessibilityEvents = true;
+            mpWindowImpl->mpFrame->Show(false);
+        }
+
+        CompatStateChanged(StateChangedType::Visible);
+
+        if (mpWindowImpl->mbReallyVisible)
+        {
+            if (mpWindowImpl->mpClippingState->mbInitWinClipRegion)
+                clipping::initWinClipRegion(*this);
+
+            vcl::Region aInvRegion = mpWindowImpl->mpClippingState->maWinClipRegion;
+
+            if (!xWindow->mpWindowImpl)
+                return;
+
+            bRealVisibilityChanged = mpWindowImpl->mbReallyVisible;
+            ImplResetReallyVisible();
+            vcl::clipping::setClipFlag(*this);
+
+            if (ImplIsOverlapWindow() && !mpWindowImpl->mbFrame)
+            {
+                // convert focus
+                if (!(nFlags & ShowFlags::NoFocusChange) && HasChildPathFocus())
+                {
+                    if (mpWindowImpl->mpOverlapWindow->IsEnabled()
+                        && mpWindowImpl->mpOverlapWindow->IsInputEnabled()
+                        && !mpWindowImpl->mpOverlapWindow->IsInModalMode())
+                        mpWindowImpl->mpOverlapWindow->GrabFocus();
+                }
+            }
+
+            if (!mpWindowImpl->mbFrame)
+            {
+                if (mpWindowImpl->mpWinData && mpWindowImpl->mpWinData->mbEnableNativeWidget)
+                {
+                    /*
+                    * #i48371# native theming: some themes draw outside the control
+                    * area we tell them to (bad thing, but we cannot do much about it ).
+                    * On hiding these controls they get invalidated with their window rectangle
+                    * which leads to the parts outside the control area being left and not
+                    * invalidated. Workaround: invalidate an area on the parent, too
+                    */
+                    const int workaround_border = 5;
+                    tools::Rectangle aBounds(aInvRegion.GetBoundRect());
+                    aBounds.AdjustLeft(-workaround_border);
+                    aBounds.AdjustTop(-workaround_border);
+                    aBounds.AdjustRight(workaround_border);
+                    aBounds.AdjustBottom(workaround_border);
+                    aInvRegion = aBounds;
+                }
+                if (!mpWindowImpl->mbNoParentUpdate)
+                {
+                    if (!aInvRegion.IsEmpty())
+                        ImplInvalidateParentFrameRegion(aInvRegion);
+                }
+                ImplGenerateMouseMove();
+            }
+        }
+    }
+    else
+    {
+        // inherit native widget flag for form controls
+        // required here, because frames never show up in the child hierarchy - which should be fixed...
+        // eg, the drop down of a combobox which is a system floating window
+        if (mpWindowImpl->mbFrame && GetParent() && !GetParent()->isDisposed()
+            && GetParent()->IsCompoundControl()
+            && GetParent()->IsNativeWidgetEnabled() != IsNativeWidgetEnabled()
+            && !(GetStyle() & WB_TOOLTIPWIN))
+        {
+            EnableNativeWidget(GetParent()->IsNativeWidgetEnabled());
+        }
+
+        if (mpWindowImpl->mbCallMove)
+        {
+            ImplCallMove();
+        }
+        if (mpWindowImpl->mbCallResize)
+        {
+            ImplCallResize();
+        }
+
+        CompatStateChanged(StateChangedType::Visible);
+
+        vcl::Window* pTestParent;
+        if (ImplIsOverlapWindow())
+            pTestParent = mpWindowImpl->mpOverlapWindow;
+        else
+            pTestParent = ImplGetParent();
+        if (mpWindowImpl->mbFrame || pTestParent->mpWindowImpl->mbReallyVisible)
+        {
+            // if a window becomes visible, send all child windows a StateChange,
+            // such that these can initialise themselves
+            ImplCallInitShow();
+
+            // If it is a SystemWindow it automatically pops up on top of
+            // all other windows if needed.
+            if (ImplIsOverlapWindow())
+            {
+                if (!(nFlags & ShowFlags::NoActivate))
+                {
+                    ImplStartToTop((nFlags & ShowFlags::ForegroundTask) ? ToTopFlags::ForegroundTask
+                                                                        : ToTopFlags::NONE);
+                    ImplFocusToTop(ToTopFlags::NONE, false);
+
+                    if (!(nFlags & ShowFlags::ForegroundTask))
+                    {
+                        // Inform user about window if we did not popup it at foreground
+                        FlashWindow();
+                    }
+                }
+            }
+
+            // adjust mpWindowImpl->mbReallyVisible
+            bRealVisibilityChanged = !mpWindowImpl->mbReallyVisible;
+            ImplSetReallyVisible();
+
+            // assure clip rectangles will be recalculated
+            vcl::clipping::setClipFlag(*this);
+
+            if (!mpWindowImpl->mbFrame)
+            {
+                InvalidateFlags nInvalidateFlags = InvalidateFlags::Children;
+                if (!IsPaintTransparent())
+                    nInvalidateFlags |= InvalidateFlags::NoTransparent;
+                ImplInvalidate(nullptr, nInvalidateFlags);
+                ImplGenerateMouseMove();
+            }
+        }
+
+        if (mpWindowImpl->mpBorderWindow)
+            mpWindowImpl->mpBorderWindow->Show(true, nFlags);
+        else if (mpWindowImpl->mbFrame)
+        {
+            // #106431#, hide SplashScreen
+            ImplSVData* pSVData = ImplGetSVData();
+            if (!pSVData->mpIntroWindow)
+            {
+                // The right way would be just to call this (not even in the 'if')
+                auto pApp = GetpApp();
+                if (pApp)
+                    pApp->InitFinished();
+            }
+            else if (!ImplIsWindowOrChild(pSVData->mpIntroWindow))
+            {
+                // ... but the VCL splash is broken, and it needs this
+                // (for ./soffice .uno:NewDoc)
+                pSVData->mpIntroWindow->Hide();
+            }
+
+            //SAL_WARN_IF( mpWindowImpl->mbSuppressAccessibilityEvents, "vcl", "Window::Show() - Frame reactivated");
+            mpWindowImpl->mbSuppressAccessibilityEvents = false;
+
+            mpWindowImpl->mbPaintFrame = true;
+            if (!Application::IsHeadlessModeEnabled())
+            {
+                bool bNoActivate(nFlags & (ShowFlags::NoActivate | ShowFlags::NoFocusChange));
+                mpWindowImpl->mpFrame->Show(true, bNoActivate);
+            }
+            if (!xWindow->mpWindowImpl)
+                return;
+
+            // Query the correct size of the window, if we are waiting for
+            // a system resize
+            if (mpWindowImpl->mbWaitSystemResize)
+            {
+                const Size aOutSize = mpWindowImpl->mpFrame->GetClientSize();
+                ImplHandleResize(this, aOutSize.Width(), aOutSize.Height());
+            }
+
+            if (mpWindowImpl->mpFrameData->mpBuffer
+                && mpWindowImpl->mpFrameData->mpBuffer->GetOutputSizePixel()
+                       != GetOutputSizePixel())
+                // Make sure that the buffer size matches the window size, even if no resize was needed.
+                mpWindowImpl->mpFrameData->mpBuffer->SetOutputSizePixel(GetOutputSizePixel());
+        }
+
+        if (!xWindow->mpWindowImpl)
+            return;
+
+        ImplShowAllOverlaps();
+    }
+
+    if (!xWindow->mpWindowImpl)
+        return;
+
+    // the SHOW/HIDE events also serve as indicators to send child creation/destroy events to the access bridge
+    // However, the access bridge only uses this event if the data member is not NULL (it's kind of a hack that
+    // we re-use the SHOW/HIDE events this way, with this particular semantics).
+    // Since #104887#, the notifications for the access bridge are done in Impl(Set|Reset)ReallyVisible. Here, we
+    // now only notify with a NULL data pointer, for all other clients except the access bridge.
+    if (!bRealVisibilityChanged)
+        CallEventListeners(mpWindowImpl->mbVisible ? VclEventId::WindowShow
+                                                   : VclEventId::WindowHide);
+}
+
+void Window::ImplSetReallyVisible()
+{
+    // #i43594# it is possible that INITSHOW was never send, because the visibility state changed between
+    // ImplCallInitShow() and ImplSetReallyVisible() when called from Show()
+    // mbReallyShown is a useful indicator
+    if (!mpWindowImpl->mbReallyShown)
+        ImplCallInitShow();
+
+    bool bBecameReallyVisible = !mpWindowImpl->mbReallyVisible;
+
+    GetOutDev()->mbDevOutput = true;
+    mpWindowImpl->mbReallyVisible = true;
+    mpWindowImpl->mbReallyShown = true;
+
+    // the SHOW/HIDE events serve as indicators to send child creation/destroy events to the access bridge.
+    // For this, the data member of the event must not be NULL.
+    // Previously, we did this in Window::Show, but there some events got lost in certain situations. Now
+    // we're doing it when the visibility really changes
+    if (bBecameReallyVisible && ImplIsAccessibleCandidate())
+        CallEventListeners(VclEventId::WindowShow, this);
+    // TODO. It's kind of a hack that we're re-using the VclEventId::WindowShow. Normally, we should
+    // introduce another event which explicitly triggers the Accessibility implementations.
+
+    vcl::Window* pWindow = mpWindowImpl->mpHierarchy->mpFirstOverlap;
+    while (pWindow)
+    {
+        if (pWindow->mpWindowImpl->mbVisible)
+            pWindow->ImplSetReallyVisible();
+        pWindow = pWindow->mpWindowImpl->mpHierarchy->mpNext;
+    }
+
+    pWindow = mpWindowImpl->mpHierarchy->mpFirstChild;
+    while (pWindow)
+    {
+        if (pWindow->mpWindowImpl->mbVisible)
+            pWindow->ImplSetReallyVisible();
+        pWindow = pWindow->mpWindowImpl->mpHierarchy->mpNext;
+    }
+}
+
+} // end vcl namespace
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
