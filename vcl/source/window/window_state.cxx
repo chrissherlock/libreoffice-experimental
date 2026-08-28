1db0fda9aaf8cb6042bfcdee3e4d38de163b1474
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-08-28 21:48:33 +1000

    vcl: extract window enablement and state logic into window_state.cxx
    Extract the highly cohesive block of functions handling window
    enablement, input states, and activation modes from the monolithic
    window.cxx into a dedicated window_state.cxx file.
    
    This reduces the size of window.cxx by nearly 200 lines and isolates
    the complex, tightly-coupled logic surrounding Enable(), EnableInput(),
    AlwaysEnableInput(), and SetActivateMode(). This continues the effort
    of breaking down the massive Window class implementation into logical,
    manageable source files.
    
    Change-Id: Id6448aa0d9f4262e36efe60ef03350ba8700e89f

---
+++ vcl/source/window/window_state.cxx
@@ -0,0 +1,283 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <sal/config.h>
+#include <comphelper/diagnose_ex.hxx>
+
+#include <vcl/event.hxx>
+#include <vcl/sysdata.hxx>
+#include <vcl/syswin.hxx>
+#include <vcl/vclevent.hxx>
+#include <vcl/window.hxx>
+
+#include <window.h>
+#include <brdwin.hxx>
+#include <salframe.hxx>
+#include <salobj.hxx>
+#include <svdata.hxx>
+
+namespace vcl
+{
+void Window::Enable(bool bEnable, bool bChild)
+{
+    if (isDisposed())
+        return;
+
+    if (!bEnable)
+    {
+        // the tracking mode will be stopped or the capture will be stolen
+        // when a window is disabled,
+        if (IsTracking())
+            EndTracking(TrackingEventFlags::Cancel);
+
+        if (IsMouseCaptured())
+            ReleaseMouse();
+
+        // try to pass focus to the next control
+        // if the window has focus and is contained in the dialog control
+        // mpWindowImpl->mbDisabled should only be set after a call of ImplDlgCtrlNextWindow().
+        // Otherwise ImplDlgCtrlNextWindow() should be used
+        if (HasFocus())
+            ImplDlgCtrlNextWindow();
+    }
+
+    if (mpWindowImpl->mpBorderWindow)
+    {
+        mpWindowImpl->mpBorderWindow->Enable(bEnable, false);
+        if ((mpWindowImpl->mpBorderWindow->GetType() == WindowType::BORDERWINDOW)
+            && static_cast<ImplBorderWindow*>(mpWindowImpl->mpBorderWindow.get())->mpMenuBarWindow)
+            static_cast<ImplBorderWindow*>(mpWindowImpl->mpBorderWindow.get())
+                ->mpMenuBarWindow->Enable(bEnable);
+    }
+
+    // #i56102# restore app focus win in case the
+    // window was disabled when the frame focus changed
+    ImplSVData* pSVData = ImplGetSVData();
+    if (bEnable && pSVData->mpWinData->mpFocusWin == nullptr
+        && mpWindowImpl->mpFrameData->mbHasFocus && mpWindowImpl->mpFrameData->mpFocusWin == this)
+        pSVData->mpWinData->mpFocusWin = this;
+
+    if (mpWindowImpl->mbDisabled != !bEnable)
+    {
+        mpWindowImpl->mbDisabled = !bEnable;
+        if (mpWindowImpl->mpSysObj)
+            mpWindowImpl->mpSysObj->Enable(bEnable && !mpWindowImpl->mbInputDisabled);
+        CompatStateChanged(StateChangedType::Enable);
+
+        CallEventListeners(bEnable ? VclEventId::WindowEnabled : VclEventId::WindowDisabled);
+    }
+
+    if (bChild)
+    {
+        VclPtr<vcl::Window> pChild = mpWindowImpl->mpHierarchy->mpFirstChild;
+        while (pChild)
+        {
+            pChild->Enable(bEnable, bChild);
+            pChild = pChild->mpWindowImpl->mpHierarchy->mpNext;
+        }
+    }
+
+    if (IsReallyVisible())
+        ImplGenerateMouseMove();
+}
+
+void Window::EnableInput(bool bEnable, bool bChild)
+{
+    if (!mpWindowImpl)
+        return;
+
+    if (mpWindowImpl->mpBorderWindow)
+    {
+        mpWindowImpl->mpBorderWindow->EnableInput(bEnable, false);
+        if ((mpWindowImpl->mpBorderWindow->GetType() == WindowType::BORDERWINDOW)
+            && static_cast<ImplBorderWindow*>(mpWindowImpl->mpBorderWindow.get())->mpMenuBarWindow)
+            static_cast<ImplBorderWindow*>(mpWindowImpl->mpBorderWindow.get())
+                ->mpMenuBarWindow->EnableInput(bEnable);
+    }
+
+    if ((!bEnable && mpWindowImpl->meAlwaysInputMode != AlwaysInputEnabled) || bEnable)
+    {
+        // automatically stop the tracking mode or steal capture
+        // if the window is disabled
+        if (!bEnable)
+        {
+            if (IsTracking())
+                EndTracking(TrackingEventFlags::Cancel);
+            if (IsMouseCaptured())
+                ReleaseMouse();
+        }
+
+        if (mpWindowImpl->mbInputDisabled != !bEnable)
+        {
+            mpWindowImpl->mbInputDisabled = !bEnable;
+            if (mpWindowImpl->mpSysObj)
+                mpWindowImpl->mpSysObj->Enable(!mpWindowImpl->mbDisabled && bEnable);
+        }
+    }
+
+    // #i56102# restore app focus win in case the
+    // window was disabled when the frame focus changed
+    ImplSVData* pSVData = ImplGetSVData();
+    if (bEnable && pSVData->mpWinData->mpFocusWin == nullptr
+        && mpWindowImpl->mpFrameData->mbHasFocus && mpWindowImpl->mpFrameData->mpFocusWin == this)
+        pSVData->mpWinData->mpFocusWin = this;
+
+    if (bChild)
+    {
+        VclPtr<vcl::Window> pChild = mpWindowImpl->mpHierarchy->mpFirstChild;
+        while (pChild)
+        {
+            pChild->EnableInput(bEnable, bChild);
+            pChild = pChild->mpWindowImpl->mpHierarchy->mpNext;
+        }
+    }
+
+    if (IsReallyVisible())
+        ImplGenerateMouseMove();
+}
+
+void Window::EnableInput(bool bEnable, const vcl::Window* pExcludeWindow)
+{
+    if (!mpWindowImpl)
+        return;
+
+    EnableInput(bEnable);
+
+    // pExecuteWindow is the first Overlap-Frame --> if this
+    // shouldn't be the case, then this must be changed in dialog.cxx
+    if (pExcludeWindow)
+        pExcludeWindow = pExcludeWindow->ImplGetFirstOverlapWindow();
+    vcl::Window* pSysWin = mpWindowImpl->mpFrameWindow->mpWindowImpl->mpFrameData->mpFirstOverlap;
+    while (pSysWin)
+    {
+        // Is Window in the path from this window
+        if (ImplGetFirstOverlapWindow()->ImplIsWindowOrChild(pSysWin, true))
+        {
+            // Is Window not in the exclude window path or not the
+            // exclude window, then change the status
+            if (!pExcludeWindow || !pExcludeWindow->ImplIsWindowOrChild(pSysWin, true))
+                pSysWin->EnableInput(bEnable);
+        }
+        pSysWin = pSysWin->mpWindowImpl->mpHierarchy->mpNextOverlap;
+    }
+
+    // enable/disable floating system windows as well
+    vcl::Window* pFrameWin = ImplGetSVData()->maFrameData.mpFirstFrame;
+    while (pFrameWin)
+    {
+        if (pFrameWin->ImplIsFloatingWindow())
+        {
+            // Is Window in the path from this window
+            if (ImplGetFirstOverlapWindow()->ImplIsWindowOrChild(pFrameWin, true))
+            {
+                // Is Window not in the exclude window path or not the
+                // exclude window, then change the status
+                if (!pExcludeWindow || !pExcludeWindow->ImplIsWindowOrChild(pFrameWin, true))
+                    pFrameWin->EnableInput(bEnable);
+            }
+        }
+        pFrameWin = pFrameWin->mpWindowImpl->mpFrameData->mpNextFrame;
+    }
+
+    // the same for ownerdraw floating windows
+    if (!mpWindowImpl->mbFrame)
+        return;
+
+    ::std::vector<VclPtr<vcl::Window>>& rList = mpWindowImpl->mpFrameData->maOwnerDrawList;
+    for (auto const& elem : rList)
+    {
+        // Is Window in the path from this window
+        if (ImplGetFirstOverlapWindow()->ImplIsWindowOrChild(elem, true))
+        {
+            // Is Window not in the exclude window path or not the
+            // exclude window, then change the status
+            if (!pExcludeWindow || !pExcludeWindow->ImplIsWindowOrChild(elem, true))
+                elem->EnableInput(bEnable);
+        }
+    }
+}
+
+void Window::AlwaysEnableInput(bool bAlways, bool bChild)
+{
+    if (mpWindowImpl->mpBorderWindow)
+        mpWindowImpl->mpBorderWindow->AlwaysEnableInput(bAlways, false);
+
+    if (bAlways && mpWindowImpl->meAlwaysInputMode != AlwaysInputEnabled)
+    {
+        mpWindowImpl->meAlwaysInputMode = AlwaysInputEnabled;
+        EnableInput(true, false);
+    }
+    else if (!bAlways && mpWindowImpl->meAlwaysInputMode == AlwaysInputEnabled)
+    {
+        mpWindowImpl->meAlwaysInputMode = AlwaysInputNone;
+    }
+
+    if (bChild)
+    {
+        VclPtr<vcl::Window> pChild = mpWindowImpl->mpHierarchy->mpFirstChild;
+        while (pChild)
+        {
+            pChild->AlwaysEnableInput(bAlways, bChild);
+            pChild = pChild->mpWindowImpl->mpHierarchy->mpNext;
+        }
+    }
+}
+
+void Window::SetActivateMode(ActivateModeFlags nMode)
+{
+    if (mpWindowImpl->mpBorderWindow)
+        mpWindowImpl->mpBorderWindow->SetActivateMode(nMode);
+
+    if (mpWindowImpl->mnActivateMode == nMode)
+        return;
+
+    mpWindowImpl->mnActivateMode = nMode;
+
+    // possibly trigger Deactivate/Activate
+    if (mpWindowImpl->mnActivateMode != ActivateModeFlags::NONE)
+    {
+        if ((mpWindowImpl->mbActive || (GetType() == WindowType::BORDERWINDOW))
+            && !HasChildPathFocus(true))
+        {
+            mpWindowImpl->mbActive = false;
+            Deactivate();
+        }
+    }
+    else
+    {
+        if (!mpWindowImpl->mbActive || (GetType() == WindowType::BORDERWINDOW))
+        {
+            mpWindowImpl->mbActive = true;
+            Activate();
+        }
+    }
+}
+
+void Window::SetUpdateMode(bool bUpdate)
+{
+    if (mpWindowImpl)
+    {
+        mpWindowImpl->mbNoUpdate = !bUpdate;
+        CompatStateChanged(StateChangedType::UpdateMode);
+    }
+}
+
+} /* namespace vcl */
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
