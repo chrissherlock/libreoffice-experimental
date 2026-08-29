4a76113c68c905edf43662c72d1034a2efc4947c
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-08-29 20:32:05 +1000

    vcl: rename paint.cxx to window_paint.cxx
    Rename paint.cxx to window_paint.cxx to maintain consistency with the
    newly established window_*.cxx naming convention in the VCL module.
    
    Update Library_vcl.mk to reflect the new compilation unit name, and
    update solenv/clang-format/excludelist to replace the old paint.cxx
    entry. Additionally, remove the leftover winproc.cxx entry from the
    clang-format excludelist following its recent rename.
    
    Change-Id: Ieca4053fb01ed8ffe1190bb0e8a1ef065e745253

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -183,7 +183,6 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/window/clipping_subsystem \
     vcl/source/window/errinf \
     vcl/source/window/settings \
-    vcl/source/window/paint \
     vcl/source/window/abstdlg \
     vcl/source/window/accel \
     vcl/source/window/accmgr \
@@ -243,6 +242,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/window/window \
     vcl/source/window/window_core \
     vcl/source/window/window_output \
+    vcl/source/window/window_paint \
     vcl/source/window/window_help \
     vcl/source/window/window_text \
     vcl/source/window/window_state \
