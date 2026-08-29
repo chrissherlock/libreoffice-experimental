47f5e27331df73cb64c8566ad821d97a77457a1b
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-08-29 20:09:37 +1000

    vcl: move docking window methods to window_docking.cxx
    Extract the docking-related methods (GetDockingManager, EnableDocking,
    ImplGetOwnerDrawList, and IsDockingWindow) from window2.cxx into a new
    dedicated source file, window_docking.cxx, and register it in the
    makefile. This continues the ongoing effort to modularize the VCL
    window implementation and clean up window2.cxx.
    
    Change-Id: I519a6dd53c795c175423fe2b2253cfafd122edbc

---
+++ vcl/source/window/window_docking.cxx
@@ -0,0 +1,51 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <vcl/dockwin.hxx>
+#include <vcl/vclptr.hxx>
+#include <vcl/window.hxx>
+
+#include <window.h>
+#include <svdata.hxx>
+
+#include <vector>
+
+namespace vcl
+{
+DockingManager* Window::GetDockingManager() { return ImplGetDockingManager(); }
+
+void Window::EnableDocking(bool bEnable)
+{
+    // update list of dockable windows
+    if (bEnable)
+        ImplGetDockingManager()->AddWindow(this);
+    else
+        ImplGetDockingManager()->RemoveWindow(this);
+}
+
+// retrieves the list of owner draw decorated windows for this window hierarchy
+::std::vector<VclPtr<vcl::Window>>& Window::ImplGetOwnerDrawList()
+{
+    return ImplGetTopmostFrameWindow()->mpWindowImpl->mpFrameData->maOwnerDrawList;
+}
+
+bool Window::IsDockingWindow() const { return mpWindowImpl && mpWindowImpl->mbDockWin; }
+} // end vcl namespace
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
