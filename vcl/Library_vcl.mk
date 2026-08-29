1f7f420792fe390a6d747436ed309b35239462ae
Christopher Sherlock <chris.sherlock79@gmail.com>
2026-08-29 13:11:26 +1000

    vcl: extract UNO interface and clipboard methods to window_uno.cxx
    Move the UNO peer interface and clipboard management functions out of
    the monolithic window.cxx file into a dedicated window_uno.cxx source
    file.
    
    This extraction groups SetWindowPeer, GetComponentInterface,
    SetComponentInterface, SetClipboard, and GetClipboard into a single
    compilation unit. By isolating these functions and fully qualifying the
    com::sun::star namespaces, we separate the heavy UNO API bindings from
    the core C++ windowing logic, reducing the size of window.cxx and
    improving separation of concerns.
    
    The new compilation unit has been properly registered in
    vcl/Library_vcl.mk.
    
    Change-Id: Ib25f81f1c85efb5058bfad11f426a6868bb22b13

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -249,6 +249,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/window/window_event \
     vcl/source/window/window_visibility \
     vcl/source/window/window_style \
+    vcl/source/window/window_uno \
     vcl/source/window/window2 \
     vcl/source/window/window3 \
     vcl/source/window/window \
